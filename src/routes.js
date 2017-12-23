f4580996cd85d8bfeb505f2642a6af8997812e23
Juho Vepsalainen <bebraw@gmail.com>
2017-12-23 11:05:21 +0200

    chore: Update to use content v8

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,40 @@
 // @flow
 const express = require("express");
 const graphql = require("express-graphql");
-const schema2018 = require("@react-finland/content-2018/src/schema");
+const { makeExecutableSchema } = require("graphql-tools");
+const {
+  schema: { typeDefs },
+  content,
+} = require("@react-finland/content-2018");
 
 function createRouter() {
   const router = new express.Router();
 
-  // Enable graphql requests.
   router.all(
     "/graphql-2018",
     graphql({
       graphiql: process.env.NODE_ENV === "development",
       pretty: true,
-      schema: schema2018,
+      schema: makeExecutableSchema({
+        typeDefs,
+        resolvers: {
+          Query: generateQueries(),
+        },
+      }),
     })
   );
 
   return router;
+}
+
+function generateQueries() {
+  const ret = {};
+
+  Object.keys(content).forEach(k => {
+    ret[k] = () => Object.values(content[k]);
+  });
+
+  return ret;
 }
 
 module.exports = createRouter;
