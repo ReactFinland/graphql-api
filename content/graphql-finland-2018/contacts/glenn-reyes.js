b7d160242e08d041d19233574247d2599e2ece4b
Mikhail Novikov <freiksenet@gmail.com>
2018-06-18 11:06:24 +0300

    Fix name

--- content/graphql-finland-2018/contacts/glenn-reyes.js
+++ content/graphql-finland-2018/contacts/glenn-reyes.js
@@ -4,7 +4,7 @@ const keywords = require("../keywords");
 module.exports = {
   name: "Glenn Reyes",
   about:
-    "Glen is a front-end Engineer, who builds modern user interfaces and apps with React & GraphQL, loves OSS, co-organizes ReactVienna meetups, and plays JM music on guitar.",
+    "Glenn is a front-end Engineer, who builds modern user interfaces and apps with React & GraphQL, loves OSS, co-organizes ReactVienna meetups, and plays JM music on guitar.",
   image: "graphql-finland-2018/speakers/glenn.jpg",
   social: {
     homepage: "https://glennreyes.com/",
