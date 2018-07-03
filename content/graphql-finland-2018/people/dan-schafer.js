6804cacbb86251d1303222c804a4568adf53ec2d
Juho Vepsalainen <bebraw@gmail.com>
2018-07-03 11:36:29 +0300

    chore: Add Dan Schafer

---
+++ content/graphql-finland-2018/people/dan-schafer.js
@@ -0,0 +1,22 @@
+const enums = require("../../../src/enums");
+const keywords = require("../keywords");
+
+module.exports = {
+  name: "Dan Schafer",
+  about:
+    "Dan Schafer is a software engineer at Facebook and a co-creator of GraphQL. He worked on the original GraphQL API for Facebook News Feed, developed Facebook’s GraphQL API Design principles, expanded GraphQL to support Mutations and Subscriptions, and contributed to the initial release of the GraphQL specification and reference implementation. Currently, he’s a tech lead on the Product Foundation team at Facebook, focused on building end-to-end client frameworks for development of the Facebook apps.",
+  image: "graphql-finland-2018/speakers/dan.png",
+  social: {
+    twitter: "dlschafer",
+    github: "dschafer",
+  },
+  location: {
+    country: {
+      name: "United States",
+      code: "US",
+    },
+    city: "San Francisco",
+  },
+  keywords: [keywords.GRAPHQL],
+  type: [enums.SPEAKER, enums.PRESENTATION, enums.KEYNOTE],
+};
