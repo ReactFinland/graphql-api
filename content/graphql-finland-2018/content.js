f2be01fbc6b2aca6196d90e45c809aa6096e677c
Juho Vepsalainen <bebraw@gmail.com>
2018-07-09 09:02:29 +0200

    chore: List Agent conf as a GraphQL Finland partner

--- content/graphql-finland-2018/content.js
+++ content/graphql-finland-2018/content.js
@@ -2,6 +2,7 @@ const people = require("./people");
 
 const allSponsors = require("../sponsors");
 const partners = [
+  allSponsors.agentconf,
   allSponsors.halfstack,
   allSponsors.reactalicante,
   allSponsors.reactdayberlin,
