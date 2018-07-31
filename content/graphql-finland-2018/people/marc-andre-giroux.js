dea16eff35e36d15abd9db2bbca803fd3b47357d
Mikhail Novikov <freiksenet@gmail.com>
2018-07-31 15:45:03 +0300

    UPD

--- content/graphql-finland-2018/people/marc-andre-giroux.js
+++ content/graphql-finland-2018/people/marc-andre-giroux.js
@@ -6,7 +6,7 @@ module.exports = {
   about:
     "A Jazz guitarist turned developer, Marc-André is currently working at GitHub, where he is trying to help building better APIs. He is currently writing a book, [The Little Book of GraphQL Design](https://book.graphqlschemadesign.com/). When he's not hacking on or thinking about GraphQL, you might find him throwing heavy weights above his head.",
   aboutShort:
-    "A Jazz guitarist turned developer, Marc-André is currently working at GitHub, where he is trying to help building better APIs. He is currently writing a book, [The Little Book of GraphQL Design](https://book.graphqlschemadesign.com/).",
+    "A Jazz guitarist turned developer, Marc-André is currently working at GitHub, where he is trying to help building better APIs. He is currently writing a book, The Little Book of GraphQL Design.",
   image: "graphql-finland-2018/speakers/marc.jpg",
   social: {
     homepage: "http://mgiroux.me/",
