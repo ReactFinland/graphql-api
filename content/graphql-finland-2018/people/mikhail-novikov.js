aa9c85aac1f479f65a631c53de3b990ba439f616
Mikhail Novikov <freiksenet@gmail.com>
2018-07-31 15:27:49 +0300

    Better short descs

--- content/graphql-finland-2018/people/mikhail-novikov.js
+++ content/graphql-finland-2018/people/mikhail-novikov.js
@@ -5,6 +5,8 @@ module.exports = {
   name: "Mikhail Novikov",
   about:
     "Mikhail is a GraphQL hipster. He started GraphQL startup before GraphQL was ever public. The startup failed, but Mikhail has been active in GraphQL community ever since, working on tools like Schema Stitching and Apollo Launchpad. He is one of the organizers of GraphQL Finland.",
+  aboutShort:
+    "Mikhail is a GraphQL hipster. He started GraphQL startup before GraphQL was ever public. After that he worked on tools like Schema Stitching and Apollo Launchpad and taugh GraphQL at multiple workshops.",
   image: "graphql-finland-2018/organizers/mikhail.jpg",
   social: {
     homepage: "https://freiksenet.com",
