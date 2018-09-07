182cb1a18d963b7b2ab9a77dc84c7b75178b4d06
Mikhail Novikov <freiksenet@gmail.com>
2018-09-07 11:44:11 +0300

    Update workshop desc

--- content/graphql-finland-2018/sessions/sara-vierra-lets-graphql.js
+++ content/graphql-finland-2018/sessions/sara-vierra-lets-graphql.js
@@ -5,17 +5,42 @@ module.exports = {
   type: enums.WORKSHOP,
   speakers: [require("../people/sara-vieira")],
   title: "Let’s Learn GraphQL",
-  description: `You signed up for this workshop because you are probably super excited about GraphQL and don’t know where to start.
-That’s exactly why GraphQL Finland has this workshop, so that you can learn some GraphQL before diving into the conference.
+  description: `
+So you want to learn GraphQL? That is awesome! But like any other technology it's not easy to get started and get the basics of it mainly because all the docs are spread all over the place.
 
-Here we will cover:
+In this 8 hour workshop we will create a GraphQL server with Apollo Server, connect it to a Postgres database in Heroku to have a fully functional GraphQL Server.
 
-- Creating A GraphQL Server in Node and connecting it to a MongoDB database
-- Using Prisma to easily create a GraphQL server
-- Using GraphQL in the Frontend with React for getting data and also for managing local state
-- If there is anyone who wants to learn the same concepts in Vue we can go over that too
+After that we will jump to the frontend using React and Apollo to query and mutate a GraphQL Endpoint easily.
 
-You will leave knowing the basics of GraphQL and ready to enjoy the rest of the conference and build your awesome API’s and Apps using this amazing technology.
+## Requirements
+
+- Node installed, I have version 10.3.0 but anything that supports async await is great!
+- I use yarn but npm is 💯
+- Git
+- Heroku Account - [https://heroku.com/](https://heroku.com/)
+- Postgres DB in Heroku - Video on how to do it: [https://cdn.rawgit.com/SaraVieira/graphql-workshop/13d73f3b/howtoheroku.mp4](https://cdn.rawgit.com/SaraVieira/graphql-workshop/13d73f3b/howtoheroku.mp4)
+- CodeSanbox Account is preferred so you can keep all the files in your account
+- Javascript knowledge
+- React knowledge
+
+## Backend
+
+- An overview of what GraphQL is
+- The basics of a querying with GraphQL
+- The basics of a mutations in GraphQL
+- Connect to a Postgres DB
+- Using context
+- Make multiple requests in one Query
+
+## Frontend
+
+- Making simple queries with React and React-Apollo
+- Making advanced queries with React and React-Apollo
+- Creating our own query component
+- Mutations with React Apollo
+- Updating the UI automatically after a mutation
+- Using pagination
+- Questions
 
 This workshop is free. Tickets would be distributed through a diversity ticket program. Ticket will include a free ticket to the actual conference. [Apply for a diversity ticket](https://diversitytickets.org/events/184).
 `,
