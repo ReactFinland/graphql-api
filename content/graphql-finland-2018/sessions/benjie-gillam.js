2048f8034173b072b5896b24fe4d76abf893a93a
Benjie Gillam <benjie@jemjie.com>
2018-09-21 12:55:42 +0100

    Tweak talk description

--- content/graphql-finland-2018/sessions/benjie-gillam.js
+++ content/graphql-finland-2018/sessions/benjie-gillam.js
@@ -5,7 +5,7 @@ const speaker = require("../people/benjie-gillam");
 module.exports = {
   speakers: [speaker],
   title: "Database-first GraphQL Development",
-  description: `Learn by example how a database-centric approach to GraphQL API development can give your engineers more time to focus on the important parts of your application. Topics covered include authentication, authorization, adhering to GraphQL best practices, and avoiding common pitfalls.`,
+  description: `Learn how a database-centric approach to GraphQL API development can give your engineers more time to focus on the important parts of your application. Topics covered include authorization, adhering to GraphQL best practices, embracing the power of PostgreSQL, and avoiding common pitfalls.`,
   type: enums.LIGHTNING_TALK,
   keywords: [keywords.GRAPHQL],
 };
