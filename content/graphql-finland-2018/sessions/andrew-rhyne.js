f6e09923b4df966453dc8f4bfd4c56f83f2fca8d
Mikhail Novikov <freiksenet@gmail.com>
2018-10-08 14:29:22 +0300

    Andrew Rhyne descr

--- content/graphql-finland-2018/sessions/andrew-rhyne.js
+++ content/graphql-finland-2018/sessions/andrew-rhyne.js
@@ -5,7 +5,8 @@ const speaker = require("../people/andrew-rhyne");
 module.exports = {
   speakers: [speaker],
   title: "Real-time GraphQL with Event Sourcing",
-  description: "",
+  description:
+    "GraphQL makes real-time easy with it's Subscriptions support, but how easy is it to build a backend that puts real-time data first?  With event sourcing, it's easier than you think!  The ability to easily subscribe to data changes and have blazing-fast reads on our data sets at Gatsby enables our team to build a sophisticated cloud-native offering that is flexible for even the most demanding product requirements.  Join Andrew as he gives a thirty-thousand foot overview of event sourcing and how naturally it fits with GraphQL and subscriptions.",
   type: enums.LIGHTNING_TALK,
   keywords: [keywords.GRAPHQL],
 };
