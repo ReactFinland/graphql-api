27b21c828e41fbca148006abcb42028d8258505d
Mikhail Novikov <freiksenet@gmail.com>
2018-10-09 09:39:08 +0300

    Ellie day

--- content/graphql-finland-2018/sessions/ellie-day.js
+++ content/graphql-finland-2018/sessions/ellie-day.js
@@ -4,9 +4,9 @@ const speaker = require("../people/ellie-day");
 
 module.exports = {
   speakers: [speaker],
-  title:
-    "GraphQL Best Practices to deliver successful front-end rewrites ahead of schedule",
-  description: "",
+  title: "Emerging uses of GraphQL for rapid application development",
+  description:
+    "GraphQL makes dealing with application data a breeze, especially with open source libraries such as Apollo. However, many schemas simply mirror their RESTful counterparts, limiting their overall usefulness. In this talk I will cover emerging patterns that enable rapid GraphQL-powered application development.",
   type: enums.TALK,
   keywords: [keywords.GRAPHQL],
 };
