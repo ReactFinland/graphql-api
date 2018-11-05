18cef16daeb78994e92aa125b86e03d977f06ecf
Juho Vepsalainen <bebraw@gmail.com>
2018-11-05 18:57:13 +0400

    chore: Add React India to sponsors

---
+++ content/sponsors/reactindia.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: "React India",
+  about: "The international React.js conference in Goa",
+  image: "sponsors/react-india.svg",
+  social: {
+    homepage: "https://www.reactindia.io/",
+    twitter: "react_india",
+    facebook: "ReactJSIndia",
+  },
+  location: {
+    country: {
+      name: "India",
+      code: "IN",
+    },
+    city: "Goa",
+  },
+};
