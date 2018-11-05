5cadd17c968593b862a1bcb0ab7a149d65bafb36
Juho Vepsalainen <bebraw@gmail.com>
2018-11-05 17:23:14 +0400

    fix(schema): Fix getSpeakers

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-const { flatMap, pick } = require("lodash");
+const { flatMap, get, pick } = require("lodash");
 const { makeExecutableSchema } = require("graphql-tools");
 const conferences = require("./conferences");
 const series = require("./conferenceSeries");
@@ -423,7 +423,7 @@ const typeDefs = `
 `;
 
 function getSpeakers(sessions) {
-  return flatMap(sessions, { speakers });
+  return flatMap(sessions, session => get(session, "speakers"));
 }
 
 module.exports = makeExecutableSchema({
