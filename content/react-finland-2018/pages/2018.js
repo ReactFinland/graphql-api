1cc07a4c7269f6da9a4a6a3dbf05f9807bd56937
Juho Vepsalainen <bebraw@gmail.com>
2018-11-06 20:25:01 +0200

    refactor: Drop old page content from the API

--- content/react-finland-2018/pages/2018.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  id: "2018",
-  title: "2018",
-  description: "React Finland 2018",
-  main: `
-React Finland was a great success! Read about [what lessons we learned](https://medium.com/react-finland/react-finland-2018-lessons-learned-754a5f29067c) and [how people experienced the conference](https://medium.com/react-finland/react-finland-2018-blog-coverage-e65bcece8946).
-    
-See also [2018 speakers](/2018/speakers/), [schedule](/2018/schedule/), and [workshops](/2018/workshops/).`,
-};
--- content/react-finland-2018/pages/workshops.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  id: "workshops",
-  title: "Workshops",
-  description: "What workshops will React Finland have?",
-};
