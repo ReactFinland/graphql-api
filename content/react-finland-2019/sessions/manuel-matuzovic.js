583198fe041a5afa17c8cb9d608044188cef1de3
Juho Vepsalainen <bebraw@gmail.com>
2018-11-13 17:20:54 +0100

    fix: Add a missing stub

---
+++ content/react-finland-2019/sessions/manuel-matuzovic.js
@@ -0,0 +1,11 @@
+const enums = require("../../../src/enums");
+const keywords = require("../../keywords");
+const speaker = require("../../people/manuel-matuzovic");
+
+module.exports = {
+  speakers: [speaker],
+  title: "Mysterious accessibility talk",
+  description: ``,
+  type: enums.TALK,
+  keywords: [],
+};
