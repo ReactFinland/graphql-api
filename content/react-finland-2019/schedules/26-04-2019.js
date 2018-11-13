3091a8d1af7e856a346b8a524278dc36d2089300
Juho Vepsalainen <bebraw@gmail.com>
2018-11-13 20:17:22 +0100

    chore: Add Varya to schedule

--- content/react-finland-2019/schedules/26-04-2019.js
+++ content/react-finland-2019/schedules/26-04-2019.js
@@ -11,6 +11,7 @@ module.exports = {
         require("../sessions/kadi-kraman"),
         require("../sessions/manuel-matuzovic"),
         require("../sessions/sia-karamalegos"),
+        require("../sessions/varya-stepanova"),
       ],
     },
   ],
