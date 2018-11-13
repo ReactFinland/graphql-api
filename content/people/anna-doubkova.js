87d52025e107124880ad41976c86d1a20be5a4f0
Juho Vepsalainen <bebraw@gmail.com>
2018-11-13 16:57:20 +0100

    chore: Add Anna Doubkova to the schedule

---
+++ content/people/anna-doubkova.js
@@ -0,0 +1,21 @@
+const enums = require("../../src/enums");
+const keywords = require("../keywords");
+
+module.exports = {
+  name: "Anna Doubková",
+  about: "Senior software engineer at Hive.",
+  image: "people/anna.jpg",
+  social: {
+    twitter: "lithinn",
+    github: "lithin",
+  },
+  location: {
+    country: {
+      name: "United Kingdom",
+      code: "UK",
+    },
+    city: "London",
+  },
+  keywords: [],
+  type: [enums.SPEAKER],
+};
