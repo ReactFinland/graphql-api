ef31df0fb412122ac629ffdcb42f42c0597e8bac
Juho Vepsalainen <bebraw@gmail.com>
2018-11-13 17:04:33 +0100

    chore: Add Tejas Kumar to speakers

---
+++ content/people/tejas-kumar.js
@@ -0,0 +1,22 @@
+const enums = require("../../src/enums");
+const keywords = require("../keywords");
+
+module.exports = {
+  name: "Tejas Kumar",
+  about: "I write code and love life.",
+  image: "people/tejas.jpg",
+  social: {
+    homepage: "http://sapegin.me/",
+    twitter: "TejasKumar_",
+    github: "tejasq",
+  },
+  location: {
+    country: {
+      name: "Germany",
+      code: "DE",
+    },
+    city: "Berlin",
+  },
+  keywords: [keywords.REACT, keywords.TOOLING],
+  type: [enums.SPEAKER],
+};
---
+++ content/react-finland-2019/sessions/tejas-kumar.js
@@ -0,0 +1,11 @@
+const enums = require("../../../src/enums");
+const keywords = require("../../keywords");
+const speaker = require("../../people/tejas-kumar");
+
+module.exports = {
+  speakers: [speaker],
+  title: "Mysterious case study",
+  description: ``,
+  type: enums.TALK,
+  keywords: [],
+};
