97462fc8ffc73adf0e4322e06fc6105968575f5a
Juho Vepsalainen <bebraw@gmail.com>
2018-11-14 09:50:55 +0100

    chore: Add Monica Lent

---
+++ content/react-finland-2019/sessions/monica-lent.js
@@ -0,0 +1,11 @@
+const enums = require("../../../src/enums");
+const keywords = require("../../keywords");
+const speaker = require("../../people/monica-lent");
+
+module.exports = {
+  speakers: [speaker],
+  title: "Mysterious design talk",
+  description: ``,
+  type: enums.TALK,
+  keywords: [],
+};
