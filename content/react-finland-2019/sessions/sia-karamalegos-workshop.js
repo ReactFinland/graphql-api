103c69b878472c749f812bf8c906cbaf130092ba
Juho Vepsalainen <bebraw@gmail.com>
2018-11-15 15:10:58 +0000

    chore: Update workshop

--- content/react-finland-2019/sessions/sia-karamalegos-workshop.js
+++ content/react-finland-2019/sessions/sia-karamalegos-workshop.js
@@ -3,10 +3,8 @@ const enums = require("../../../src/enums");
 module.exports = {
   type: enums.WORKSHOP,
   speakers: [require("../../people/sia-karamalegos")],
-  title: "Universal React Apps Using Next.js",
-  description: `Every user’s hardware is different, and processing speed can hinder user experience on client-side rendered React applications. Server-side rendering and code-splitting can drastically improve user experience by minimizing the work that the client has to do.
-
-  It’s easy to get lost in the lingo, so come learn what it all means and how to easily build universal React apps using the Next.js framework. We’ll walk through the concepts and use code examples to cement your understanding. You’ll get the most out of this session if you’re comfortable with React and ES6 syntax.`,
+  title: "React Performance",
+  description: ``,
   /*tickets: [
     {
       ...require("../tickets/morning-workshop")[0],
