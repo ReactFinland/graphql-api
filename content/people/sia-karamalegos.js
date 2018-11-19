8271f61e271c4e3bbe8780d93831fbf775c36b89
Sia <siakaramalegos@gmail.com>
2018-11-19 15:37:24 -0600

    Update my bio

--- content/people/sia-karamalegos.js
+++ content/people/sia-karamalegos.js
@@ -4,7 +4,7 @@ const keywords = require("../keywords");
 module.exports = {
   name: "Sia Karamalegos",
   about:
-    "Sia Karamalegos is the founder and lead developer for Clio + Calliope Web Development. She has over 15 years of experience in technology, strategy, project management, and operations from small startups to large corporations across multiple industries, especially high-tech and education. She leverages her depth of experience with software engineering to build high-value applications.",
+    "Sia Karamalegos is a web developer, currently focused on developing web applications and improving front-end performance. She is an international conference speaker and co-organizes #FrontEndParty, gnocode, and NOLA Hack Night in the New Orleans area. Sia loves making developers' lives easier through teaching, speaking at conferences, and mentoring. She is the founder and lead developer for Clio + Calliope Web Development and was recognized in the Silicon Bayou 100, the 100 most influential and active people in tech and entrepreneurship in Louisiana. When she's not coding, speaking, or consulting, Sia likes to design crochet patterns and dabble in charcoal figure drawing. She's also an avid endurance athlete.",
   image: "people/sia.jpg",
   social: {
     homepage: "https://siakaramalegos.github.io/",
