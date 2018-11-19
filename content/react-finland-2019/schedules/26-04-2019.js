8dad5056145155dc8f8743e400e11b4e1df85e12
Juho Vepsalainen <bebraw@gmail.com>
2018-11-19 20:12:17 +0100

    chore: Add opening of the day

--- content/react-finland-2019/schedules/26-04-2019.js
+++ content/react-finland-2019/schedules/26-04-2019.js
@@ -1,9 +1,23 @@
+const enums = require("../../../src/enums");
+
 module.exports = {
   day: "2019-04-26",
   description: "Presentation day",
   intervals: [
     {
       begin: "09:00",
+      end: "09:15",
+      sessions: [
+        {
+          speakers: [require("../../people/sara-vieira")],
+          title: "Opening",
+          description: "Opening the day with Sara Vieira, our MC",
+          type: enums.ORGANIZATIONAL,
+        },
+      ],
+    },
+    {
+      begin: "09:15",
       end: "18:00",
       sessions: [
         require("../sessions/anna-doubkova"),
