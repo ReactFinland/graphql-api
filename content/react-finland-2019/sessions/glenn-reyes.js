654229a93b7278e52cd2b955f0611cf4cba68b97
Juho Vepsalainen <bebraw@gmail.com>
2018-11-20 09:30:10 +0100

    fix: Fix speaker reference

--- content/react-finland-2019/sessions/glenn-reyes.js
+++ content/react-finland-2019/sessions/glenn-reyes.js
@@ -1,6 +1,6 @@
 const enums = require("../../../src/enums");
 const keywords = require("../../keywords");
-const speaker = require("../../people/artem-sapegin");
+const speaker = require("../../people/glenn-reyes");
 
 module.exports = {
   speakers: [speaker],
