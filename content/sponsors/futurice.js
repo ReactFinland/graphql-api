b6494b3abf572f67ba100da9d53dd2f75ca0cafd
Juho Vepsalainen <bebraw@gmail.com>
2018-11-29 19:28:56 +0100

    chore: Add Futurice

---
+++ content/sponsors/futurice.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "Futurice",
+  about: "You can’t predict the future. Are you prepared to define it?",
+  image: "sponsors/futurice.svg",
+  social: {
+    homepage: "https://www.futurice.com/",
+    facebook: "futurice",
+    github: "futurice",
+    linkedin: "company/futurice",
+    youtube: "",
+    instagram: "",
+    twitter: "futurice",
+    pinterest: "",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Helsinki",
+  },
+};
