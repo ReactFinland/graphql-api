02fc0b7467bf70ae74e8ff0a718e3340db890d39
Juho Vepsalainen <bebraw@gmail.com>
2018-11-29 19:22:09 +0100

    chore: Add Columbia Road

---
+++ content/sponsors/columbia-road.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "Columbia Road",
+  about: "A Digital Growth Consultancy by Futurice",
+  image: "sponsors/columbia-road.png",
+  social: {
+    homepage: "https://www.columbiaroad.com/",
+    facebook: "ColumbiaRoadCom",
+    github: "",
+    linkedin: "company/columbia-road",
+    youtube: "channel/UC5v0xp5cyUIcNCkcXqLEOLA",
+    instagram: "columbiaroadcom",
+    twitter: "ColumbiaRoadCom",
+    pinterest: "",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Helsinki",
+  },
+};
