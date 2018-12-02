3416d73354ff1f94be236186c22d978830697295
Bruno Lourenco <brunnolou@gmail.com>
2018-12-02 22:32:48 +0000

    Add Bruno talk and description

--- content/people/bruno-lourenco.js
+++ content/people/bruno-lourenco.js
@@ -3,7 +3,8 @@ const keywords = require("../keywords");
 
 module.exports = {
   name: "Bruno Lourenço",
-  about: "",
+  about:
+    "Bruno Lourenço has loved animations since Flash. He’s passionate about physics-based UI animations and believes coding them doesn’t have to be complicated - which led him to create React-Morph. He’s a Senior Frontend Developer at Ginetta, a design agency based in Switzerland.",
   image: "people/bruno.jpg",
   social: {
     homepage: "",
@@ -18,6 +19,6 @@ module.exports = {
     },
     city: "Braga",
   },
-  keywords: [keywords.REACT],
+  keywords: [keywords.ANIMATION, keywords.REACT],
   type: [enums.SPEAKER],
 };
--- content/react-finland-2019/sessions/bruno-lourenco.js
+++ content/react-finland-2019/sessions/bruno-lourenco.js
@@ -4,8 +4,11 @@ const speaker = require("../../people/bruno-lourenco");
 
 module.exports = {
   speakers: [speaker],
-  title: "Mysterious animation talk",
-  description: ``,
+	title: "Delightful UI animations by understanding the brain",
+	description: `Check out the world’s award-winning websites—what do they all have in common? Animations! They have elements that move and change. When done well, animations can amaze us.
+But why are some animations delightful while others are so annoying? The answer could be in how our brain perceives motion.
+Understanding this could be the key to confidently adding animations to our websites and apps.
+Animations with code seem hard, but they shouldn’t be. In this talk, I will give some tips and explore some tools to simplify creating animations in React.`,
   type: enums.TALK,
   keywords: [],
 };
