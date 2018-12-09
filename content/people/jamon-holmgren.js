ceba65eb784e71d0bc4383c1ce71211447f33843
Juho Vepsalainen <bebraw@gmail.com>
2018-12-09 17:40:53 +0100

    chore: Add Jamon Holmgren to schedule

---
+++ content/people/jamon-holmgren.js
@@ -0,0 +1,28 @@
+const enums = require("../../src/enums");
+const keywords = require("../keywords");
+
+module.exports = {
+  name: "Jamon Holmgren",
+  about:
+    "Founder & CTO @infinitered (http://infinite.red). Software engineer (JS, React/React Native, Node, Ruby, Elixir), Lutheran, husband, dad to 4.",
+  image: "people/jamon.jpg",
+  social: {
+    homepage: "https://jamonholmgren.com/",
+    twitter: "jamonholmgren",
+    github: "jamonholmgren",
+    facebook: "",
+    medium: "",
+    instagram: "",
+    linkedin: "",
+    youtube: "",
+  },
+  location: {
+    country: {
+      name: "United States",
+      code: "US",
+    },
+    city: "Vancouver",
+  },
+  keywords: [keywords.REACT, keywords.REACT_NATIVE],
+  type: [enums.SPEAKER],
+};
---
+++ content/react-finland-2019/sessions/jamon-holmgren.js
@@ -0,0 +1,11 @@
+const enums = require("../../../src/enums");
+const keywords = require("../../keywords");
+const speaker = require("../../people/jamon-holmgren");
+
+module.exports = {
+  speakers: [speaker],
+  title: "Mysterious Ignite talk",
+  description: ``,
+  type: enums.TALK,
+  keywords: [keywords.REACT_NATIVE],
+};
