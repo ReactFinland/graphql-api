b911cd6f32d849a0b013ab5e18407e27c5a170f2
Mikhail Novikov <freiksenet@gmail.com>
2018-12-12 11:56:36 +0200

    Update jani-evakallio.js

--- content/people/jani-evakallio.js
+++ content/people/jani-evakallio.js
@@ -3,8 +3,7 @@ const keywords = require("../keywords");
 
 module.exports = {
   name: "Jani Eväkallio",
-  about:
-    "Jani, a veteran of the Frontend Framework wars, has earned his battle scars writing JavaScript on the bleeding edge for the last decade. Today, he leads a team building cross-stack React mobile and web apps at Formidable London.\n\nHe gets childishly excited about beautiful user interactions and design tools. In his spare time, he performs improvised comedy and speaks at conferences advocating for the React ecosystem, particularly React Native and GraphQL.",
+  about: "Jani is a small-town Finnish boy far from home. Today he leads a team building cross-stack React mobile and web apps at Formidable London and, in his spare time, performs improvised comedy and speaks at conferences advocating for the React ecosystem, particularly React Native and GraphQL.",
   image: "people/jani.jpg",
   social: {
     homepage: "",
