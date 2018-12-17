51a641550d193183b65fae3ca4a05239bdccab50
Eemeli Aro <eemeli@gmail.com>
2018-12-17 22:43:21 +0200

    chore: Add Vincit (#29)

---
+++ content/sponsors/vincit.js
@@ -0,0 +1,21 @@
+module.exports = {
+  name: "Vincit",
+  about: "Not Another Software Company",
+  image: "sponsors/vincit.svg",
+  social: {
+    homepage: "https://www.vincit.fi/",
+    facebook: "VincitOyj",
+    github: "vincit",
+    linkedin: "company/vincit-oy",
+    twitter: "VincitOy",
+    youtube: "VincitOyTampere",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Tampere",
+  },
+};
+
