67033da5c5de1606e231eec67d925b74567a30c5
Juho Vepsalainen <bebraw@gmail.com>
2019-01-04 14:46:42 +0100

    chore: Add Wunder

---
+++ content/sponsors/wunder.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: "Wunder",
+  about: "Your partner in digital change",
+  image: "sponsors/wunder.svg",
+  social: {
+    homepage: "https://wunder.io/",
+    facebook: "wunder.io",
+    twitter: "Wunder_io",
+    linkedin: "company/wunder.io",
+    youtube: "UCsBoaJt5sX12IFBu1giXZJg",
+    instagram: "wunder_fi",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Helsinki",
+  },
+};
