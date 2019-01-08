c848d765ab9cb429a7a2c87c58e869895ccf333d
Juho Vepsalainen <bebraw@gmail.com>
2019-01-08 15:22:31 +0100

    chore: Add typeof conference

---
+++ content/sponsors/typeof.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "typeof",
+  about: "typeof is a web craftsmanship conference",
+  image: "sponsors/typeof.svg",
+  social: {
+    homepage: "https://typeofconf.com/",
+    facebook: "typeofconf",
+    twitter: "typeofconf",
+    instagram: "typeofconf",
+  },
+  location: {
+    country: {
+      name: "Portugal",
+      code: "PT",
+    },
+    city: "Porto",
+  },
+};
