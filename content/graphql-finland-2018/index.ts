5a971c14b812875140f60559a44553ebc8008db8
Juho Vepsalainen <bebraw@gmail.com>
2019-01-14 17:42:14 +0100

    fix: Fix conference indices
    It's better not to generate these. Maybe the generator should be dotfile
    based?

--- content/graphql-finland-2018/index.ts
+++ content/graphql-finland-2018/index.ts
@@ -1,1 +1,59 @@
-export { default as schedules } from "./schedules";
+import * as locations from "../locations";
+import * as people from "../people";
+import * as allSponsors from "../sponsors";
+import * as schedules from "./schedules";
+import * as sessions from "./sessions";
+import * as tickets from "./tickets";
+
+const partners = [
+  allSponsors.agentconf,
+  allSponsors.apiops,
+  allSponsors.designsystemslondon,
+  allSponsors.elisa,
+  allSponsors.halfstack,
+  allSponsors.osaango,
+  allSponsors.postgraphile,
+  allSponsors.reactalicante,
+  allSponsors.reactdayberlin,
+  allSponsors.reactjsday,
+  allSponsors.reactnativeeu,
+  allSponsors.survivejs,
+  allSponsors.webexpo,
+  allSponsors.yglfkyiv,
+  allSponsors.zapier,
+];
+const goldSponsors = [allSponsors.digia, allSponsors.gofore];
+const silverSponsors = [allSponsors.alma];
+const bronzeSponsors = [
+  allSponsors.amazon,
+  allSponsors.apollo,
+  allSponsors.codento,
+  allSponsors.gatsby,
+  allSponsors.hasura,
+  allSponsors.prisma,
+];
+const sponsors = partners.concat(goldSponsors, silverSponsors, bronzeSponsors);
+
+export default {
+  id: "graphql-finland-2018",
+  name: "GraphQL Finland 2018",
+  year: "2018",
+  websiteUrl: "https://graphql-finland.fi/",
+  locations: [locations],
+  organizers: [
+    people["aleksi-pousar"],
+    people["harri-maatta"],
+    people["juho-vepsalainen"],
+    people["mikhail-novikov"],
+    people["samuli-hakoniemi"],
+    people["tuuli-tiilikainen"],
+  ],
+  schedules,
+  sessions,
+  tickets,
+  sponsors,
+  partners,
+  goldSponsors,
+  silverSponsors,
+  bronzeSponsors,
+};
--- content/react-finland-2018/index.ts
+++ content/react-finland-2018/index.ts
@@ -1,1 +1,58 @@
-export { default as schedules } from "./schedules";
+import * as locations from "../locations";
+import * as people from "../people";
+import * as allSponsors from "../sponsors";
+import * as schedules from "./schedules";
+import * as sessions from "./sessions";
+import * as tickets from "./tickets";
+
+const partners = [
+  allSponsors.agentconf,
+  allSponsors.halfstack,
+  allSponsors.reactalicante,
+  allSponsors.reactdayberlin,
+  allSponsors.reactfest,
+  allSponsors.reactjsday,
+  allSponsors.reactnativeeu,
+  allSponsors.reasonconf,
+  allSponsors.survivejs,
+  allSponsors.webexpo,
+  allSponsors.zapier,
+];
+const goldSponsors = [allSponsors.gofore, allSponsors.solita];
+const silverSponsors = [
+  allSponsors.elisa,
+  allSponsors.motley,
+  allSponsors.nitor,
+];
+const bronzeSponsors = [
+  allSponsors.alma,
+  allSponsors.geniem,
+  allSponsors.rohea,
+  allSponsors.verkkokauppa,
+];
+const sponsors = partners.concat(goldSponsors, silverSponsors, bronzeSponsors);
+
+export default {
+  id: "react-finland-2018",
+  name: "React Finland 2018",
+  year: "2018",
+  websiteUrl: "https://react-finland.fi/",
+  locations: [locations["elisa-appelsiini"], locations["valkoinen-sali"]],
+  organizers: [
+    people["aarni-koskela"],
+    people["aleksi-pousar"],
+    people["harri-maatta"],
+    people["joni-nevalainen"],
+    people["juho-vepsalainen"],
+    people["mikhail-novikov"],
+    people["samuli-hakoniemi"],
+  ],
+  schedules,
+  sessions,
+  tickets,
+  sponsors,
+  partners,
+  goldSponsors,
+  silverSponsors,
+  bronzeSponsors,
+};
