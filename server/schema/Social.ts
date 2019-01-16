70b841ed707ea441fb25b81259814ac7f108fcb5
Juho Vepsalainen <bebraw@gmail.com>
2019-01-16 14:54:07 +0100

    fix: Allow social fields to be nullable

--- server/schema/Social.ts
+++ server/schema/Social.ts
@@ -2,30 +2,30 @@ import { Field, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class Social {
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public homepage?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public twitter?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public github?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public facebook?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public medium?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public instagram?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public linkedin?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public youtube?: string;
 
-  @Field(_ => String)
+  @Field(_ => String, { nullable: true })
   public vk?: string;
 }
