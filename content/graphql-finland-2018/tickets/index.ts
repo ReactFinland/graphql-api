2f3d928d35a1fa691751a7a57c1ea9f24efc6996
Juho Vepsalainen <bebraw@gmail.com>
2019-01-16 14:37:25 +0100

    chore: Drop tickets data
    Not needed at the moment.

--- content/graphql-finland-2018/tickets/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as conference } from "./conference";
-export { default as workshop } from "./workshop";
--- content/react-finland-2018/tickets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as conference } from "./conference";
-export { default as dayWorkshop } from "./day-workshop";
-export { default as eveningWorkshop } from "./evening-workshop";
-export { default as morningWorkshop } from "./morning-workshop";
