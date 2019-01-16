6d69ef9c725d4649e892b7e8c165fcfdd564f8ab
Juho Vepsalainen <bebraw@gmail.com>
2019-01-16 14:36:22 +0100

    fix: Port missing file

--- content/react-finland-2018/workshops/david-khourshid-workshop.ts
+++ content/react-finland-2018/workshops/david-khourshid-workshop.ts
@@ -1,28 +1,15 @@
 import { SessionType } from "../../../server/schema";
+import { Session } from "../../../server/schema/Session";
+import { elisaAppelsiini } from "../../locations";
+import david from "../../people/david-khourshid";
 
-export default {
+const workshop: Session = {
   type: SessionType.WORKSHOP,
-  speakers: [require("../../people/david-khourshid")],
+  people: [david],
   title: "Reactive State Machines and Statecharts",
   description: `Managing the many user interface states of an application easily becomes complicated. We'll discover how some historical and important computer science concepts – state machines and statecharts – and a functional + reactive approach can make it much easier to understand, visualize, implement, and automatically create tests for complex user interfaces and flows.`,
-  tickets: [
-    {
-      ...require("../tickets/morning-workshop")[0],
-      link: "https://ti.to/react-finland/2018/with/meyhplcurh8",
-    },
-    {
-      ...require("../tickets/morning-workshop")[1],
-      link: "https://ti.to/react-finland/2018/with/e5849f-srk8",
-    },
-    {
-      ...require("../tickets/evening-workshop")[0],
-      link: "https://ti.to/react-finland/2018/with/mdzr1bwzkr8",
-    },
-    {
-      ...require("../tickets/evening-workshop")[1],
-      link: "https://ti.to/react-finland/2018/with/duwuhyzgmq8",
-    },
-  ],
-  keywords: require("../../people/david-khourshid").keywords,
-  location: require("../../locations")["elisa-appelsiini"],
+  keywords: david.keywords,
+  location: elisaAppelsiini,
 };
+
+export default workshop;
