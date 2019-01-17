4de89d5e832108dc572321982f7b496270fbc997
Juho Vepsalainen <bebraw@gmail.com>
2019-01-17 15:23:59 +0100

    chore: Add React Norway and React New York

---
+++ content/sponsors/reactnewyork.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "React New York",
+  about: "The international event for coding inspiration",
+  image: "sponsors/reactnewyork.svg",
+  social: {
+    homepage: "https://reactnewyork.com/",
+    facebook: "",
+    instagram: "",
+    twitter: "reactnewyork",
+  },
+  location: {
+    country: {
+      name: "United States",
+      code: "US",
+    },
+    city: "New York",
+  },
+};
