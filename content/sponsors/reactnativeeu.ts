e04bc098a13af69c152aeff9abada82f31596484
Juho Vepsalainen <bebraw@gmail.com>
2019-01-22 14:39:53 +0100

    chore: Update RNEU logo

--- content/sponsors/reactnativeeu.ts
+++ content/sponsors/reactnativeeu.ts
@@ -5,7 +5,7 @@ const sponsor: Contact = {
   about:
     "First community conference in Europe, focused exclusively on React Native. Community, core contributors, insights, networking and tons of knowledge.",
   image: {
-    url: "sponsors/react-native-eu.png",
+    url: "sponsors/react-native-eu.svg",
   },
   social: {
     homepage: "https://react-native.eu/",
