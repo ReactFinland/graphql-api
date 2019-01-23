49adfce66a531c61c99fea356372d4c8b087341f
Juho Vepsalainen <bebraw@gmail.com>
2019-01-23 11:22:32 +0100

    chore: Drop build directory from source control
    Given Heroku is managed by a CI now, we shouldn't need build directory
    anymore.

--- build/content/locations/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var elisa_appelsiini_1 = require("./elisa-appelsiini");
-exports.elisaAppelsiini = elisa_appelsiini_1.default;
-var paasitorni_1 = require("./paasitorni");
-exports.paasitorni = paasitorni_1.default;
-var valkoinen_sali_1 = require("./valkoinen-sali");
-exports.valkoinenSali = valkoinen_sali_1.default;
-//# sourceMappingURL=index.js.map
