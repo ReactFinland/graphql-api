f44463d31f5cc2890f300008d2302fc8ad23fd86
Juho Vepsalainen <bebraw@gmail.com>
2019-01-23 14:40:17 +0100

    chore: Add Meiko as a sponsor

---
+++ content/sponsors/meiko.ts
@@ -0,0 +1,24 @@
+import { Contact } from "../../server/schema/Contact";
+
+const sponsor: Contact = {
+  name: "Meiko",
+  about: "Tailored digital advantage",
+  image: {
+    url: "sponsors/meiko.svg",
+  },
+  social: {
+    homepage: "https://meiko.fi/",
+    facebook: "",
+    instagram: "",
+    twitter: "",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Jyväskylä",
+  },
+};
+
+export default sponsor;
