66623ed21b5c68351618c91043b10155a6592eaf
Monica Lent <lent.monica@gmail.com>
2019-01-28 12:00:52 +0100

    Edit talk topic, edit speaker bio, add architecture keyword. (#44)

--- content/people/monica-lent.ts
+++ content/people/monica-lent.ts
@@ -5,7 +5,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "Monica Lent",
   about:
-    "Frontend Lead @SumUp. Balcony-gardener, and taco fanatic from Tucson, Arizona. I like cycling, craft beer, picnics, and knitwear. IPAs ⟩ IDEs.",
+    "Monica is the Frontend Lead at SumUp, a multinational FinTech startup based in Berlin. She began coding at the tender age of ten years old, and somehow managed to pivot from studying Latin in university to where she is today, leading a team of developers distributed in three countries. Before moving abroad, Monica worked in domains such as natural language processing and cloud technologies.",
   image: {
     url: "people/monica.jpg",
   },
@@ -22,7 +22,7 @@ const person: Contact = {
     },
     city: "Berlin",
   },
-  keywords: [Keyword.REACT, Keyword.STYLING, Keyword.TOOLING],
+  keywords: [Keyword.REACT, Keyword.ARCHITECTURE, Keyword.TOOLING],
   type: [ContactType.SPEAKER],
 };
 
--- content/react-finland-2019/talks/monica-lent.ts
+++ content/react-finland-2019/talks/monica-lent.ts
@@ -1,13 +1,14 @@
 import { SessionType } from "../../../server/schema";
 import { Session } from "../../../server/schema/Session";
 import speaker from "../../people/monica-lent";
+import Keyword from "../../../server/schema/keywords";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious design talk",
-  description: ``,
+  title: "Building resilient frontend architecture",
+  description: `Change is inevitable. So is legacy. And too often, we as developers (who love to solve problems by coding) fall into the trap of believing the only way to fix it is by rewriting everything again and again. But how can we design an application architecture that is more resilient to change in the first place? In this talk we’ll look at specific techniques you can use today to keep your app from turning into that infamous ball of mud.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [Keyword.CODE_QUALITY, Keyword.TOOLING, Keyword.ARCHITECTURE],
 };
 
 export default talk;
