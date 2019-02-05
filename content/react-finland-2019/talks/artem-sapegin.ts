bfb2cb591729a909ac814c69f59e21545f9fa934
Artem Sapegin <artem@sapegin.ru>
2019-02-05 16:36:09 +0100

    Update my talk description (#45)

--- content/react-finland-2019/talks/artem-sapegin.ts
+++ content/react-finland-2019/talks/artem-sapegin.ts
@@ -1,13 +1,14 @@
 import { SessionType } from "../../../server/schema";
 import { Session } from "../../../server/schema/Session";
 import speaker from "../../people/artem-sapegin";
+import Keyword from "../../../server/schema/keywords";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious components talk",
-  description: ``,
+  title: "Custom CSS is the path to inconsistent UI",
+  description: `Custom CSS is the simplest way to make the UI inconsistent, especially if we’re talking about spacing between UI elements, font sizes or colors. When developers have too much freedom in defining the UI, they will do things differently. The issue could be solved by avoiding any custom CSS on the app level and having a set of primitive components that allow developers to glue UI together in a consistent way.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [Keyword.REACT, Keyword.STYLING],
 };
 
 export default talk;
