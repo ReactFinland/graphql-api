b39319380640ddb4db14caa950afe8fe97348ba5
Juha Linnanen <juha.linnanen@solinor.com>
2019-02-06 11:08:10 +0200

    Update session details

--- content/react-finland-2019/talks/juha-linnanen.ts
+++ content/react-finland-2019/talks/juha-linnanen.ts
@@ -5,8 +5,8 @@ import speaker from "../../people/juha-linnanen";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious React Native talk",
-  description: ``,
+  title: "CI/CD for React Native",
+  description: `Both continuous integration and continuous delivery (CI/CD) are elements of software development best practices. Setting up CI/CD can be time consuming, even more so for a mobile project. Understanding the basics of how to apply CI/CD practices in React Native mobile application development to automate software release tasks will improve performance and will free up time more usefully spent elsewhere in the project.`,
   type: SessionType.LIGHTNING_TALK,
   keywords: [Keyword.REACT_NATIVE],
 };
