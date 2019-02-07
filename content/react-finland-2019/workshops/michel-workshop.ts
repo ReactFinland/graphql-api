67b39436d7168ff48e503c6e6bd832f1b9e1311e
Juho Vepsalainen <bebraw@gmail.com>
2019-02-07 10:12:42 +0100

    chore: Update Michel's workshop

--- content/react-finland-2019/workshops/michel-workshop.ts
+++ content/react-finland-2019/workshops/michel-workshop.ts
@@ -6,30 +6,15 @@ import { michelWeststrate } from "../../people";
 const workshop: Session = {
   type: SessionType.WORKSHOP,
   people: [michelWeststrate],
-  title: "TypeScript: From novice to expert",
+  title: "TypeScript for React Developers",
   description: `
-The JavaScript ecosystem is moving to TypeScript. Fast. This workshop will get you up and running with the basic and more advanced patterns of the language.
+  The JavaScript ecosystem is moving to TypeScript. Fast. This workshop will get you up and running with the basic and more advanced patterns of TypeScript. TypeScript has some powerful features, that fit very well with the dynamic nature of the JavaScript language. In this workshop we will cover all the essentials and some of the unique features of TypeScript.
 
-Maybe you have a background in static typed languages. In that case will find some common ground in concepts like classes, interfaces and overloading.
-
-TypeScript also has a some unique and powerful features, that fit very well with the dynamic nature of the JavaScript language. In this workshop we will cover them all.
-
-During this full-day workshop we will cover:
-
-- Getting started with TypeScript
-- TypeScript. Why? And when (not)?
-- Applying TypeScript to existing code bases.
-- The basics: Primitives, interfaces, classes, generics.
-- Slightly more fancy stuff: field modifiers, unions, overloads.
-- 'any', 'never', and 'unknown'. Your buddies for life!
-- Strictness settings.
-- The cool stuff: type guards, type discrimination, tuples and spreads, mapped types, conditional types, flow analysis, this typing, index signatures.
-- Best practices, or: going from how-to-satisfy-the-compiler to: TypeScript-makes-me-write-actually-better-code.
-- Beyond TypeScript: crossing the boundaries of static and run-time type checking.
-
-Michel has been working with TypeScript full-time for the last 3 years. He authored several open source libraries (such as MobX and MobX-state-tree) that are written in TypeScript and leverage some of it's finest features.
-
-Pre-requisites: Familiarity with JavaScript and modern syntax features (ES2015).
+  But beyond that, this is a workshop for React devs! To practice the theory, during the hands-on parts of this workshop we will be migrating an application from plain JavaScript to TypeScript. 
+  
+  About the author: Michel has been working with TypeScript full-time for the last 3 years. He authored several open source libraries (such as MobX and MobX-state-tree) that are written in TypeScript and leverage some of it's finest features.
+  
+  Pre-requisites: Familiarity with JavaScript, React and modern syntax features (ES2015).
 `,
   keywords: michelWeststrate.keywords,
   location: paasitorni,
