6d21a0540737f4bb2ea0f4898ae86200aae7d9f9
Juho Vepsalainen <bebraw@gmail.com>
2019-02-11 15:18:13 +0100

    chore: Add stubs for pages

--- server/routes/generate-assets.tsx
+++ server/routes/generate-assets.tsx
@@ -3,33 +3,65 @@ import { renderToString } from "react-dom/server";
 
 function routeAssetGenerator(router) {
   router.get("/generate-assets", (req, res) => {
-    res.status(200).send(renderMarkup(renderToString(GenerateAssetsIndex())));
+    res.status(200).send(renderMarkup(renderToString(GenerateAssets())));
+  });
+
+  router.get("/generate-assets/badges", (req, res) => {
+    res.status(200).send(renderMarkup(renderToString(GenerateBadges())));
+  });
+
+  router.get("/generate-assets/schedule", (req, res) => {
+    res.status(200).send(renderMarkup(renderToString(GenerateSchedule())));
+  });
+
+  router.get("/generate-assets/presentation", (req, res) => {
+    res.status(200).send(renderMarkup(renderToString(GeneratePresentation())));
+  });
+
+  router.get("/generate-assets/text", (req, res) => {
+    res.status(200).send(renderMarkup(renderToString(GenerateText())));
   });
 }
 
-function GenerateAssetsIndex() {
+function GenerateAssets() {
   return (
     <ul>
       <li>
-        <a href="/badges">Generate badges</a>
+        <a href="./badges">Generate badges</a>
       </li>
       <li>
-        <a href="/schedule">Generate a schedule poster</a>
+        <a href="./schedule">Generate a schedule poster</a>
       </li>
       <li>
-        <a href="/presentation#0">Generate a presentation with speakers</a>
+        <a href="./presentation#0">Generate a presentation with speakers</a>
       </li>
       <li>
-        <a href="/speakers-room">Generate a speakers' room poster</a>
+        <a href="./text#'Speakers' room">Generate a speakers' room poster</a>
       </li>
       <li>
-        <a href="/left-arrow">Generate a left arrow poster</a>
+        <a href="./text#←">Generate a left arrow poster</a>
       </li>
       <li>
-        <a href="/right-arrow">Generate a right arrow poster</a>
+        <a href="./text#→">Generate a right arrow poster</a>
       </li>
     </ul>
   );
+}
+
+function GenerateBadges() {
+  return <div>generate badges</div>;
+}
+
+function GenerateSchedule() {
+  return <div>generate schedule</div>;
+}
+
+function GeneratePresentation() {
+  return <div>generate presentation</div>;
+}
+
+function GenerateText() {
+  return <div>generate text</div>;
 }
 
 function renderMarkup(html) {
