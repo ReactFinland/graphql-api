ca924ee5f0fad3fbb1407f7de0d5a6857ada5857
Juho Vepsalainen <bebraw@gmail.com>
2019-02-14 11:28:11 +0100

    fix: Serve other than images correctly
    These go through local files instead of Cloudinary.

--- server/routes/media.ts
+++ server/routes/media.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import logger from "../logger";
 import { resolveImage } from "./resolve-image";
 
@@ -5,19 +6,29 @@ function routeMedia(router, mediaUrl, mediaPath) {
   router.all(`${mediaUrl}/*`, async (req, res, next) => {
     const asset = req.params["0"];
 
-    try {
-      const url = await resolveImage(mediaPath, asset);
+    if ([".jpg", ".png", ".svg"].includes(path.extname(asset))) {
+      try {
+        const url = await resolveImage(mediaPath, asset);
 
-      // TODO: This is where it would be possible to intercept and cache
-      // so we don't hit Cloudinary CDN.
-      // Likely we want something like images.react-finland.fi and then use
-      // that as a bucket for some CDN (Cloudflare?) to decouple images from
-      // the API.
-      res.redirect(url);
-    } catch (err) {
-      logger.error(err);
+        // TODO: This is where it would be possible to intercept and cache
+        // so we don't hit Cloudinary CDN.
+        // Likely we want something like images.react-finland.fi and then use
+        // that as a bucket for some CDN (Cloudflare?) to decouple images from
+        // the API.
+        res.redirect(url);
+      } catch (err) {
+        logger.error(err);
 
-      next();
+        next();
+      }
+    } else {
+      res.sendFile(asset, { root: mediaPath }, err => {
+        res.end();
+
+        if (err) {
+          logger.error(err);
+        }
+      });
     }
   });
 }
