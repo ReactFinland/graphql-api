22798668adfe9fc7ffb08e3fa2caa8cfafed7779
Juho Vepsalainen <bebraw@gmail.com>
2019-02-15 15:49:03 +0100

    chore(react-finland-2019): Add Carolyn Stransky's talk

--- content/react-finland-2019/talks/carolyn-stransky.ts
+++ content/react-finland-2019/talks/carolyn-stransky.ts
@@ -4,8 +4,12 @@ import speaker from "../../people/carolyn-stransky";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious talk",
-  description: ``,
+  title: '"Intuitive" Tooling',
+  description: `
+  One of the nuances of working in frontend development is constantly being told that [insert whatever tooling, frameworks or technologies here] are "super simple" or "easy to pickup". While on the surface this seems great, the reality is that many of these technologies are riddled with syntax quirks, incomplete documentation and a high barrier to entry. 
+
+  In this talk, we'll discuss why truly intuitive tooling doesn't exist, go over some common misconceptions in the React community and learn more inclusive ways to talk about emerging frontend technologies.
+  `,
   type: SessionType.TALK,
   keywords: [],
 };
