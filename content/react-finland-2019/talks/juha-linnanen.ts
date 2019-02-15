3fd22228155fa2cca3b436d6dea21bc1ec68c556
Juho Vepsalainen <bebraw@gmail.com>
2019-02-15 15:47:48 +0100

    chore(react-finland-2019): Change Juha Linnanen's session to regular talk

--- content/react-finland-2019/talks/juha-linnanen.ts
+++ content/react-finland-2019/talks/juha-linnanen.ts
@@ -1,13 +1,13 @@
 import { SessionType } from "../../../server/schema";
-import { Session } from "../../../server/schema/Session";
 import Keyword from "../../../server/schema/keywords";
+import { Session } from "../../../server/schema/Session";
 import speaker from "../../people/juha-linnanen";
 
 const talk: Session = {
   people: [speaker],
   title: "CI/CD for React Native",
   description: `Both continuous integration and continuous delivery (CI/CD) are elements of software development best practices. Setting up CI/CD can be time consuming, even more so for a mobile project. Understanding the basics of how to apply CI/CD practices in React Native mobile application development to automate software release tasks will improve performance and will free up time more usefully spent elsewhere in the project.`,
-  type: SessionType.LIGHTNING_TALK,
+  type: SessionType.TALK,
   keywords: [Keyword.REACT_NATIVE],
 };
 
