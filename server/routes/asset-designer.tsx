8af3ae56533ffeafcaffa79006a7fce12db766eb
Juho Vepsalainen <bebraw@gmail.com>
2019-02-24 11:22:11 +0100

    chore(asset-designer): Parse possible query
    This needs a bit more design.

--- server/routes/asset-designer.tsx
+++ server/routes/asset-designer.tsx
@@ -1,3 +1,4 @@
+import { validate } from "isvalid";
 import * as React from "react";
 import createInteractive from "./components/Interactive";
 import createConnection from "./create-connection";
@@ -9,35 +10,40 @@ async function routeAssetDesigner(router, schema, projectRoot, scriptRoot) {
   const Interactive = createInteractive(projectRoot, scriptRoot, __dirname);
   const connection = createConnection(schema);
 
-  router.get("/asset-designer", async (req, res) => {
-    const [err, connect] = await connection(
-      [queries.themeQuery, queries.themesQuery],
-      {
-        conferenceSeriesId: "react-finland",
-      }
-    );
-
-    if (err) {
-      return res.status(400).send();
-    }
-
-    const { theme } = connect(queries.themeQuery);
-    const { themes } = connect(queries.themesQuery);
-
-    res
-      .status(200)
-      .send(
-        renderPage(
-          req.url,
-          theme,
-          <Interactive
-            relativeComponentPath="./pages/AssetDesignerPage"
-            props={{ theme, themes }}
-            component={AssetDesignerPage}
-          />
-        )
+  router.get(
+    "/asset-designer",
+    validate.query({
+      conferenceSeriesId: { type: String, default: "react-finland" },
+    }),
+    async (req, res) => {
+      // TODO: Redirect with query visible instead of defaulting?
+      const [err, connect] = await connection(
+        [queries.themeQuery, queries.themesQuery],
+        req.query
       );
-  });
+
+      if (err) {
+        return res.status(400).send();
+      }
+
+      const { theme } = connect(queries.themeQuery);
+      const { themes } = connect(queries.themesQuery);
+
+      res
+        .status(200)
+        .send(
+          renderPage(
+            req.url,
+            theme,
+            <Interactive
+              relativeComponentPath="./pages/AssetDesignerPage"
+              props={{ theme, themes }}
+              component={AssetDesignerPage}
+            />
+          )
+        );
+    }
+  );
 }
 
 export default routeAssetDesigner;
