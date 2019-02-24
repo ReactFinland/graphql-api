8da9e046e01340cf5cac1584a030e63d97dfc6e6
Bohdan Liashenko <lyashenkoinc@gmail.com>
2019-02-24 09:58:07 +0100

    Fix grammar in description

--- content/react-finland-2019/talks/bohdan-liashenko.ts
+++ content/react-finland-2019/talks/bohdan-liashenko.ts
@@ -5,7 +5,7 @@ import speaker from "../../people/bohdan-liashenko";
 const talk: Session = {
   people: [speaker],
   title: "Codecrumbs in React",
-  description: `The granular approach of designing components helps us to build reusable and encapsulated features. On another hand, it also makes a codebase much more complex by extra added abstractions and drastically increased amount of source code files. How we can navigate the maze of hundreds of React-atoms now? Join my talk to get the answer.`,
+  description: `The granular approach of designing components helps us to build reusable and encapsulated features. On another hand, it also makes a codebase much more complex by extra added abstractions and drastically increased amount of source code files. How can we navigate the maze of hundreds of React-atoms now? Join my talk to get the answer.`,
   type: SessionType.LIGHTNING_TALK,
   keywords: [Keyword.TOOLING],
 };
