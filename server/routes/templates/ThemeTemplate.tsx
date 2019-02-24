2c5986f346edce6c9ddc7be8bec5442b83c3af7d
Juho Vepsalainen <bebraw@gmail.com>
2019-02-24 10:20:41 +0100

    refactor: Separate theme template to a file of its own

---
+++ server/routes/templates/ThemeTemplate.tsx
@@ -0,0 +1,108 @@
+import styled from "@emotion/styled";
+import { Color } from "csstype";
+import { map } from "lodash";
+import { complement, invert } from "polished";
+import * as React from "react";
+import { Theme } from "../../schema/Theme";
+
+interface ThemeTemplateProps {
+  theme: Theme;
+}
+
+const ThemeTemplateContainer = styled.main`
+  margin: 2em;
+`;
+const ThemeTemplateHeader = styled.h1``;
+
+const ColorsContainer = styled.section`
+  margin-top: 1em;
+  margin-bottom: 1em;
+`;
+const ColorHeader = styled.h2`
+  margin-bottom: 0.5em;
+`;
+
+interface ColorProps {
+  color: Color;
+}
+const Color = styled.div`
+  padding: 1em;
+  display: inline-block;
+  width: 8em;
+  height: 8em;
+  background-color: ${({ color }: ColorProps) => color};
+  color: ${({ color }: ColorProps) =>
+    tryTo(() => complement(invert(color)), "")};
+`;
+
+const LogosContainer = styled.section`
+  margin-top: 1em;
+  margin-bottom: 1em;
+`;
+const LogoHeader = styled.h2`
+  margin-bottom: 0.5em;
+`;
+
+interface LogoProps {
+  property: string;
+  src: string;
+}
+const LogoContainer = styled.section`
+  margin-right: 1em;
+  display: inline-block;
+  vertical-align: top;
+  font-style: italic;
+`;
+const Logo = styled.img`
+  max-width: 8em;
+  max-height: 8em;
+  background-color: ${({ property }: LogoProps) =>
+    tryTo(() => invert(property), "")};
+`;
+const LogoLabel = styled.label``;
+
+function tryTo(fn, defaultValue) {
+  try {
+    return fn();
+  } catch (err) {
+    return defaultValue;
+  }
+}
+
+interface ThemeTemplateProps {
+  theme: Theme;
+}
+
+function ThemeTemplate({ theme }: ThemeTemplateProps) {
+  return (
+    <ThemeTemplateContainer>
+      <ThemeTemplateHeader>Theme</ThemeTemplateHeader>
+      <ColorsContainer>
+        <ColorHeader>Colors</ColorHeader>
+        {map(theme.colors, (color, colorProperty) => (
+          <Color color={color} key={color}>
+            {colorProperty}
+          </Color>
+        ))}
+      </ColorsContainer>
+      <LogosContainer>
+        <LogoHeader>Logos</LogoHeader>
+        {map(theme.logos, (logo, logoProperty) =>
+          map(logo, (image, imageProperty) => (
+            <LogoContainer>
+              <LogoLabel>{`${logoProperty}.${imageProperty}`}</LogoLabel>
+              <Logo
+                property={logoProperty}
+                key={image.url}
+                src={image.url}
+                alt={`${logoProperty}.${imageProperty}`}
+              />
+            </LogoContainer>
+          ))
+        )}
+      </LogosContainer>
+    </ThemeTemplateContainer>
+  );
+}
+
+export default ThemeTemplate;
