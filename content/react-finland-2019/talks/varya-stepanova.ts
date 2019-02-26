d5717daa2a84de3273a9ee96c1f4828f63e8b0fc
Varya Stepanova <mail@varya.me>
2019-02-26 10:12:47 +0200

    Varya's talk name and description

--- content/react-finland-2019/talks/varya-stepanova.ts
+++ content/react-finland-2019/talks/varya-stepanova.ts
@@ -3,8 +3,13 @@ import speaker from "../../people/varya-stepanova";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious talk",
-  description: ``,
+  title: "A practical guide to building your design system infrastructure",
+  description: `Design systems bridge the gap between designers and developers, communicate shared practises to all the company levels and significantly decrease production costs. We all have heard this but how to achieve that in practice?
+
+React ecosystem and community already provide a lot of efficient open source tools which can be used for building your design system infrastructure. With them, you can have the most automated development process, high-level interactive documentation for the libraries in your system and support contribution practises within your organisation.
+
+This practical guide shows how to apply existing development tools to your system and what is the value you can get. 
+`,
   type: SessionType.TALK,
   keywords: [],
 };
