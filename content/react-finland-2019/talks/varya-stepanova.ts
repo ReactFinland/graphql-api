43f456145c5c4b6dce3071107391c8e79d3b3764
Varya Stepanova <mail@varya.me>
2019-02-26 13:28:55 +0200

    Fixing typos (#55)

--- content/react-finland-2019/talks/varya-stepanova.ts
+++ content/react-finland-2019/talks/varya-stepanova.ts
@@ -4,9 +4,9 @@ import speaker from "../../people/varya-stepanova";
 const talk: Session = {
   people: [speaker],
   title: "A practical guide to building your design system infrastructure",
-  description: `Design systems bridge the gap between designers and developers, communicate shared practises to all the company levels and significantly decrease production costs. We all have heard this but how to achieve that in practice?
+  description: `Design systems bridge the gap between designers and developers, communicate shared practices to all the company levels and significantly decrease production costs. We all have heard this but how to achieve that in practice?
 
-React ecosystem and community already provide a lot of efficient open source tools which can be used for building your design system infrastructure. With them, you can have the most automated development process, high-level interactive documentation for the libraries in your system and support contribution practises within your organisation.
+React ecosystem and community already provide a lot of efficient open source tools which can be used for building your design system infrastructure. With them, you can have the most automated development process, high-level interactive documentation for the libraries in your system and support contribution practices within your organisation.
 
 This practical guide shows how to apply existing development tools to your system and what is the value you can get. 
 `,
