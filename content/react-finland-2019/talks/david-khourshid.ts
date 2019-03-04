afbe8667479f49ef7cd83fb50c69c3a2470a09af
Juho Vepsäläinen <bebraw@gmail.com>
2019-03-04 20:23:23 +0100

    Add David K. Piano's talk (#58)
    * Update david-khourshid.ts
    * Fix: Fix keywords

--- content/react-finland-2019/talks/david-khourshid.ts
+++ content/react-finland-2019/talks/david-khourshid.ts
@@ -1,12 +1,18 @@
+import Keyword from "../../../server/schema/keywords";
 import { Session, SessionType } from "../../../server/schema/Session";
 import speaker from "../../people/david-khourshid";
 
 const talk: Session = {
   people: [speaker],
-  title: "Mysterious talk",
-  description: ``,
+  title: "Mind-Reading with Adaptive and Intelligent UIs in React",
+  description: `What if you could predict user behavior with smart UIs? In this talk, we will explore how we can make adaptive and intelligent UIs in React that learn from how individual users use your apps, and personalize the interface and features just for them, in real-time. With probability-driven statecharts, decision trees, reinforcement learning and more, UIs can be developed in such a way that it automatically adapts to the user's behavior.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [
+    Keyword.STATE_MACHINES,
+    Keyword.REINFORCEMENT_LEARNING,
+    Keyword.ARTIFICIAL_INTELLIGENCE,
+    Keyword.USER_INTERFACE,
+  ],
 };
 
 export default talk;
