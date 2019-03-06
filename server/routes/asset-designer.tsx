12cb70a2e7a7a9dd1882e2d7f05dd43319224b44
Juho Vepsalainen <bebraw@gmail.com>
2019-03-06 09:39:21 +0100

    refactor(asset-designer): Simplify router logic
    No need to validate as that can crash hard. It's better to pass through
    to keep some kind of backwards compatibility.

--- server/routes/asset-designer.tsx
+++ server/routes/asset-designer.tsx
@@ -1,14 +1,10 @@
 import expeditousEngineMemory from "expeditious-engine-memory";
 import getExpeditiousCache from "express-expeditious";
-import isvalid from "isvalid";
-import fromPairs from "lodash/fromPairs";
 import * as React from "react";
 import "reflect-metadata"; // Needed for introspecting classes
-import * as components from "./components";
 import createInteractive from "./components/Interactive";
 import AssetDesignerPage from "./pages/AssetDesignerPage";
 import renderPage from "./render-page";
-import * as templates from "./templates";
 
 const cache = getExpeditiousCache({
   namespace: "assetdesignercache",
@@ -19,87 +15,29 @@ const cache = getExpeditiousCache({
 async function routeAssetDesigner(router, schema, projectRoot, scriptRoot) {
   const Interactive = createInteractive(projectRoot, scriptRoot, __dirname);
 
-  interface AssetQuery {
-    selectionId: string;
-    variables: { [key: string]: any };
-  }
+  router.get("/asset-designer", cache.withTtl("1 hour"), async (req, res) => {
+    const selectionId: string = req.query.selectionId;
+    const variables: { [key: string]: any } = req.query.variables
+      ? JSON.parse(req.query.variables)
+      : {};
 
-  router.get(
-    "/asset-designer",
-    cache.withTtl("1 hour"),
-    async (req, res, next) => {
-      const query: AssetQuery = req.query;
-      const selectedComponent =
-        templates[query.selectionId] || components[query.selectionId];
-      const variables = getAdditionalQueryParameters(selectedComponent);
-
-      const parsedQuery = {
-        selectionId: req.query.selectionId,
-        variables: req.query.variables ? JSON.parse(req.query.variables) : {},
-      };
-
-      isvalid(parsedQuery, {
-        type: Object,
-        unknownKeys: "remove", // allow/deny/remove
-        schema: {
-          selectionId: { type: String, default: "ThemeTemplate" },
-          themeId: { type: String, default: "react-finland" },
-          variables: {
-            type: Object,
-            schema: variables,
-          },
-        },
-      })
-        .then(query => {
-          req.query = query;
-
-          next();
-        })
-        .catch(err => {
-          console.log(query, parsedQuery, err);
-
-          req.query = undefined;
-
-          next(err);
-        });
-    },
-    async (req, res) => {
-      const { selectionId, variables }: AssetQuery = req.query;
-
-      // Given we use the same page for serving different bundles,
-      // each needs a unique identifier based on the query (differing part)
-      res
-        .status(200)
-        .send(
-          renderPage(
-            "Asset designer",
-            req.url,
-            <Interactive
-              relativeComponentPath="./pages/AssetDesignerPage"
-              props={{ initialState: { selectionId, variables } }}
-              component={AssetDesignerPage}
-              componentHash={selectionId}
-            />
-          )
-        );
-    }
-  );
-}
-
-function getAdditionalQueryParameters(template) {
-  if (!template || !template.variables) {
-    return {};
-  }
-
-  return fromPairs(
-    template.variables
-      .map(({ id, validation }) =>
-        id && validation
-          ? [id, { type: validation.type, default: validation.default }]
-          : null
-      )
-      .filter(Boolean)
-  );
+    // Given we use the same page for serving different bundles,
+    // each needs a unique identifier based on the query (differing part)
+    res
+      .status(200)
+      .send(
+        renderPage(
+          "Asset designer",
+          req.url,
+          <Interactive
+            relativeComponentPath="./pages/AssetDesignerPage"
+            props={{ initialState: { selectionId, variables } }}
+            component={AssetDesignerPage}
+            componentHash={selectionId}
+          />
+        )
+      );
+  });
 }
 
 export default routeAssetDesigner;
