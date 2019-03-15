b801e181df84cb5ac2b1bc56d405b955e6af8525
Juho Vepsalainen <bebraw@gmail.com>
2019-03-15 16:03:08 +0100

    chore: Sketch out reason conf data fetching

--- content/reason-conf-2019/index.ts
+++ content/reason-conf-2019/index.ts
@@ -1,14 +1,45 @@
+import request from "request-promise-native";
 import { Conference } from "../../server/schema/Conference";
 import * as locations from "../locations";
 import * as people from "../people";
 import * as allSponsors from "../sponsors";
-import schedules from "./schedules";
 
 const sponsors = [];
 const partners = [];
 const goldSponsors = [];
 const silverSponsors = [];
 const bronzeSponsors = [];
+
+async function getData() {
+  try {
+    const { files } = await request.get(
+      "https://api.github.com/gists/992b79b40cea84ff7ca8efb8cc2350ee",
+      {
+        headers: {
+          "User-Agent": "React-Finland",
+        },
+        json: true,
+      }
+    );
+
+    return {
+      schedule: files["schedule.json"].content,
+      speakers: files["speakers.json"].content,
+    };
+  } catch (err) {
+    console.error(err);
+  }
+
+  return {};
+}
+
+async function init() {
+  const { schedule, speakers } = await getData();
+
+  console.log(schedule, speakers);
+}
+
+init();
 
 const conference: Conference = {
   id: "reason-conf-2019",
@@ -25,7 +56,7 @@ const conference: Conference = {
     people.nikGraf,
     people.patrickStapfer,
   ],
-  schedules,
+  schedules: [],
   sponsors,
   partners,
   goldSponsors,
