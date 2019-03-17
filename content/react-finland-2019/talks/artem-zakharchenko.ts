b0d3cfd7f3235a330e5db95cf8d26ab60cb14dc0
Artem Zakharchenko <kettanaito@gmail.com>
2019-03-17 17:32:13 +0100

    Adds talk title and description

--- content/react-finland-2019/talks/artem-zakharchenko.ts
+++ content/react-finland-2019/talks/artem-zakharchenko.ts
@@ -3,10 +3,10 @@ import speaker from "../../people/artem-zakharchenko";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "Creating layouts that last",
+  description: `What if we could wield layout composition as an actual React component? Spoiler: that would solve the #1 issue that prevents our layouts from being truly reusable.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [css grid, layout, atomic, react],
 };
 
 export default talk;
