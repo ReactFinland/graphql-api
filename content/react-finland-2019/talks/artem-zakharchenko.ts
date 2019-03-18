8d9091acc6f2cd9dc4914d43464f42ab041b5d2d
Juho Vepsalainen <bebraw@gmail.com>
2019-03-18 10:35:29 +0100

    chore(react-finland-2019): Add Artem Z's talk
    Closes #61.

--- content/react-finland-2019/talks/artem-zakharchenko.ts
+++ content/react-finland-2019/talks/artem-zakharchenko.ts
@@ -1,12 +1,13 @@
+import Keyword from "../../../server/schema/keywords";
 import { Session, SessionType } from "../../../server/schema/Session";
 import speaker from "../../people/artem-zakharchenko";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "Creating layouts that last",
+  description: `What if we could wield layout composition as an actual React component? Spoiler: that would solve the #1 issue that prevents our layouts from being truly reusable.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [Keyword.CSS_GRID, Keyword.LAYOUT, Keyword.REACT],
 };
 
 export default talk;
