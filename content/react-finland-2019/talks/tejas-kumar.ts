2fa8e2fb153b146cfab7c990c1374ef954079c04
Tejas Kumar <tejas@tejas.qa>
2019-03-20 18:15:39 +0100

    Add my talk

--- content/react-finland-2019/talks/tejas-kumar.ts
+++ content/react-finland-2019/talks/tejas-kumar.ts
@@ -3,10 +3,16 @@ import speaker from "../../people/tejas-kumar";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "Scalable (Design) Systems with TypeScript",
+  description: `This session explores creating living documentation for design systems, ensuring consistent documentation at all times using TypeScript.
+We will read Markdown files, renders components described in them, and expose live type-safe code playgrounds powered by the Monaco Editor. We will, together, explore:
+
+- Static Type Analysis and its value
+- The Monaco text editor that powers VS Code
+- Connecting it all together with React
+`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: ["react", "design system", "monaco editor", "vs code"],
 };
 
 export default talk;
