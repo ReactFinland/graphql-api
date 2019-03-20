6440e6bf79117280b04680a23373abc0f172a946
Juho Vepsalainen <bebraw@gmail.com>
2019-03-20 09:37:45 +0100

    chore(react-finland-2019): Update Rick's talk

--- content/react-finland-2019/talks/rick-hanlon.ts
+++ content/react-finland-2019/talks/rick-hanlon.ts
@@ -1,12 +1,13 @@
+import Keyword from "../../../server/schema/keywords";
 import { Session, SessionType } from "../../../server/schema/Session";
 import speaker from "../../people/rick-hanlon";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "React Everywhere",
+  description: `From web, to iOS, Android, and VR: React is everywhere. In this talk, Rick Hanlon from the React Native Core team reviews all of the platforms using React to build apps with React Native, and previews the latest platform to join the React Native family: Web.`,
   type: SessionType.KEYNOTE,
-  keywords: [],
+  keywords: [Keyword.REACT_NATIVE, Keyword.REACT, Keyword.REACT_VR],
 };
 
 export default talk;
