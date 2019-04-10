a8b89f99f76537ec8f55f2b3d8b1a280e975051b
Juho Vepsalainen <bebraw@gmail.com>
2019-04-10 10:33:09 +0200

    chore: Update Christoffer's talk

--- content/react-finland-2019/talks/christoffer-niska.ts
+++ content/react-finland-2019/talks/christoffer-niska.ts
@@ -5,8 +5,10 @@ import speaker from "../../people/christoffer-niska";
 const talk: Session = {
   people: [speaker],
   title: "Releasing a library written in TypeScript on NPM",
-  description: "", // Session description goes here. ES6 template and markdown work
-  type: SessionType.TALK, // SessionType.LIGHTNING_TALK, SessionType.KEYNOTE, SessionType.WORKSHOP are possible too
+  description: `Most of us have used or at least heard about TypeScript, but how many have released a React library written in TypeScript on NPM?
+
+In this session we will take a quick look at the tooling required and some best practices for publishing a library written in TypeScript.`,
+  type: SessionType.LIGHTNING_TALK,
   keywords: [Keyword.NPM, Keyword.TYPESCRIPT],
 };
 
