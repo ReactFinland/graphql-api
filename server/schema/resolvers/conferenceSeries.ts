3af860afd3c79035ed58d51b9456664994acb0f0
Juho Vepsalainen <bebraw@gmail.com>
2019-04-10 10:24:53 +0200

    chore: Use wave texture for React Finland
    Works better than flowers.

--- server/schema/resolvers/conferenceSeries.ts
+++ server/schema/resolvers/conferenceSeries.ts
@@ -117,7 +117,7 @@ const conferenceSeries: {
     conferences: ["react-finland-2018", "react-finland-2019"],
     theme: {
       id: "react-finland",
-      textures: [{ url: "/media/assets/floral.svg" }],
+      textures: [{ url: "/media/assets/wave.svg" }],
       colors: {
         primary: "#002fa9",
         secondary: "#5297e9",
