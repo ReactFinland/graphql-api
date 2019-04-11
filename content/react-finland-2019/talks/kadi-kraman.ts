df1af6cb921502590ffc83f89ca1b2195bf3767f
Kadi Kraman <hellokadi@gmail.com>
2019-04-11 09:41:43 +0100

    Add description for Kadi's talk (#69)

--- content/react-finland-2019/talks/kadi-kraman.ts
+++ content/react-finland-2019/talks/kadi-kraman.ts
@@ -1,12 +1,13 @@
 import { Session, SessionType } from "../../../server/schema/Session";
 import speaker from "../../people/kadi-kraman";
+import Keyword from "../../../server/schema/keywords";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "All aboard the type train",
+  description: `JavaScript is a language of add-ons, and one of its more recent trends is typing. There are obvious benefits to typing, but what are the drawbacks? Should you choose Flow or TypeScript? Why use types at all? And why not just go straight to Reason? This session will provide some insight into these questions, peppered with examples from the real world.`,
   type: SessionType.LIGHTNING_TALK,
-  keywords: [],
+  keywords: [Keyword.TYPESCRIPT],
 };
 
 export default talk;
--- server/schema/keywords.ts
+++ server/schema/keywords.ts
@@ -55,6 +55,7 @@ enum Keyword {
   VS_CODE = "VS Code",
   WEBPACK = "Webpack",
   WEB_ASSEMBLY = "Web Assembly",
+  TYPESCRIPT = "Typescript"
 }
 
 export default Keyword;
