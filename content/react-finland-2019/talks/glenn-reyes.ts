33959cb70bdd85347f01b9a4f7ca054577dc515b
Glenn Reyes <glenn@glennreyes.com>
2019-04-14 17:21:56 +0200

    Update glenn-reyes.ts (#71)

--- content/react-finland-2019/talks/glenn-reyes.ts
+++ content/react-finland-2019/talks/glenn-reyes.ts
@@ -5,7 +5,7 @@ import speaker from "../../people/glenn-reyes";
 const talk: Session = {
   people: [speaker],
   title: "Drawing a line between 3rd party and hand crafted components",
-  description: `Often when we write new React components, we might be thinking: Come on, there must be one out there! And most times someone already faced the same problem... but it's not quite the same. Should I use the 3rd party lib? Or go with my own? Let's find out the differences and trade offs in this session.`,
+  description: `Often when we write new React components, we might be thinking: Come on, there must be one out there! And most times someone already faced the same problem... only that it's not quite the same. Should I use the 3rd party lib? Or go with my own? Let's find out the differences and trade offs in this session.`,
   type: SessionType.LIGHTNING_TALK,
   keywords: [Keyword.REACT, Keyword.LIBRARIES],
 };
