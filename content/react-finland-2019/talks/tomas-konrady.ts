50f6125b445e1af75d050bd3bcecc25072ebdce0
Tomáš Konrády <tomaskonrady@gmail.com>
2019-04-15 14:41:42 +0200

    Adding description to talk of Tomas Konrady

--- content/react-finland-2019/talks/tomas-konrady.ts
+++ content/react-finland-2019/talks/tomas-konrady.ts
@@ -1,12 +1,23 @@
 import { Session, SessionType } from "../../../server/schema/Session";
+import Keyword from "../../../server/schema/keywords";
 import speaker from "../../people/tomas-konrady";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: ``,
+  title: "React Union: Write once, plug anywhere",
+  description: `React is usually responsible for managing the render of the whole application. This talk will be about the different use case in which a React application is divided into several standalone configurable modules.
+
+I am going to show how the React modules can be integrated into diverse CMS platforms such as WordPress or Liferay with help of React Union library.
+
+Then we take a look at state management challenges in this kind of setup and how to solve them with @redux-tools.`,
   type: SessionType.TALK,
-  keywords: [],
+  keywords: [
+    Keyword.CMS,
+    Keyword.REACT,
+    Keyword.REDUX,
+    Keyword.STATE_MANAGEMENT,
+    Keyword.LIVE_CODING,
+  ],
 };
 
 export default talk;
