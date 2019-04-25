a5a65523c4b6d78029ab9d8023554cecc736a266
Tomáš Konrády <tomaskonrady@gmail.com>
2019-04-25 22:18:45 +0300

    updated info for TK (#81)

--- content/people/tomas-konrady.ts
+++ content/people/tomas-konrady.ts
@@ -3,7 +3,7 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Tomáš Konrády",
-  about: "Developer... (and artist a bit)",
+  about: "Frontend developer at Lundegaard a.s. Lover of Ramda and creator of ramda-extension and react-union libraries.",
   image: {
     url: "people/tomas.jpg",
   },
@@ -17,7 +17,7 @@ const person: Contact = {
       name: "Czech Republic",
       code: "CZ",
     },
-    city: "Hradec Kralove",
+    city: "Hradec Králové",
   },
   keywords: [Keyword.REACT, Keyword.TOOLING],
   type: [ContactType.SPEAKER],
