207e74cce83b322d58ff8a05206dbadddb1b35ad
Juho Vepsalainen <bebraw@gmail.com>
2019-04-29 11:32:52 +0300

    chore: Add a talk description

--- content/react-finland-2019/talks/rick-hanlon.ts
+++ content/react-finland-2019/talks/rick-hanlon.ts
@@ -5,7 +5,7 @@ import speaker from "../../people/rick-hanlon";
 const talk: Session = {
   people: [speaker],
   title: "The Untouchable Web",
-  description: ``,
+  description: `The majority of users are now browsing websites primarily using touch devices. In this talk, Rick Hanlon from the React Native team at Facebook shares what React Native has learned about touch devices with web developers, including how to provide a rich user experiance today, and where this space is going.`,
   type: SessionType.KEYNOTE,
   keywords: [Keyword.REACT_NATIVE, Keyword.REACT],
 };
