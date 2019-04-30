4c469b67169cc4c4bf05bf438ade4b16b34b1db3
Juho Vepsalainen <bebraw@gmail.com>
2019-04-30 14:59:02 +0300

    chore: Set up workshop income generator

--- utils/generate-workshop-income.js
+++ utils/generate-workshop-income.js
@@ -1,18 +1,10 @@
 const fs = require("fs-extra");
 const parseCsv = require("csv-parse/lib/sync"); // TODO: Use the async version instead?
-const { groupBy, mapValues, pickBy } = require("lodash");
+const { groupBy, mapValues, sumBy } = require("lodash");
 
 generateWorkshopIncome();
 
 async function generateWorkshopIncome() {
-  const attendees = groupBy(
-    parseCsv(await fs.readFile("./attendees.csv", { encoding: "utf8" }), {
-      columns: true,
-      delimiter: ",",
-      skip_empty_lines: true,
-    }),
-    "Ticket"
-  );
   const workshopNames = [
     "TypeScript for React Developers",
     "Design Systems for React Developers",
@@ -21,26 +13,30 @@ async function generateWorkshopIncome() {
     "Advanced React – Suspense, Time Slicing, Hooks and more",
     "Get in the Fast Lane: Measuring React Performance",
   ];
-  const workshopCuts = calculateShares(
-    pickBy(attendees, (_, name) =>
-      workshopNames.some(workshopName => name.includes(workshopName))
-    )
+  const workshopCuts = mapValues(
+    groupBy(
+      parseCsv(await fs.readFile("./attendees.csv", { encoding: "utf8" }), {
+        columns: true,
+        delimiter: ",",
+        skip_empty_lines: true,
+      })
+        .map(attendee => ({
+          ...attendee,
+          Workshop: workshopNames.find(workshopName =>
+            attendee.Ticket.includes(workshopName)
+          ),
+          Cut: attendee.Ticket.includes("Workshop only")
+            ? Number(attendee.Price) / 2
+            : Number(attendee.Price) / 4,
+        }))
+        .filter(attendee => attendee.Workshop),
+      "Workshop"
+    ),
+    attendees => Math.round(sumBy(attendees, "Cut"))
   );
 
-  console.log(Object.keys(attendees).length, workshopCuts);
-}
-
-function calculateShares(ticketGroups) {
-  return mapValues(ticketGroups, attendees =>
-    attendees
-      .map(attendee => ({
-        ...attendee,
-        Cut: attendee.Ticket.includes("Workshop only")
-          ? Number(attendee.Price) / 2
-          : Number(attendee.Price) / 4,
-      }))
-      .reduce((a, b) => {
-        return a + b.Cut;
-      }, 0)
+  console.log(
+    workshopCuts,
+    Object.values(workshopCuts).reduce((a, b) => a + b, 0)
   );
 }
