97b69cc1e3e63fe84dcff067ddffb03119a87778
Juho Vepsalainen <bebraw@gmail.com>
2019-06-11 15:05:43 +0200

    chore: Drop GraphQL hack made for mobile app queries

--- server/routes/graphql.ts
+++ server/routes/graphql.ts
@@ -4,12 +4,6 @@ import process from "process";
 function routeGraphQL(router, schema, projectRoot, mediaUrl) {
   router.all(
     "/graphql",
-    (req, res, next) => {
-      // Patch for app query. Once the app is updated, this can go away.
-      req.body.query = patchQuery(req.body.query);
-
-      next();
-    },
     graphql(request => {
       const hostname = getHostname(request);
 
@@ -34,88 +28,6 @@ function getHostname(req) {
   }
 
   return req.protocol + "://" + req.get("host");
-}
-
-// FIXME: Ideally this should traverse query tree and replace
-// .. on Workshop and ... on Talk with a single query.
-function patchQuery(query) {
-  const q = `
-  {
-    conference(id: "react-finland-2019") {
-      schedules {
-        day
-        intervals {
-          begin
-          end
-          sessions {
-            title
-            description
-            type
-            ... on Workshop {
-              speakers {
-                name
-                image {
-                  url
-                }
-              }
-            }
-            ... on Talk {
-              speakers {
-                name
-                image {
-                  url
-                }
-              }
-            }
-            location {
-              name
-              city
-              address
-            }
-          }
-        }
-      }
-    }
-  }
-  `;
-
-  if (!query) {
-    return query;
-  }
-
-  if (q.replace(/(\n| )*/g, "") === query.replace(/(\n| )*/g, "")) {
-    return `
-    {
-      conference(id: "react-finland-2019") {
-        schedules {
-          day
-          intervals {
-            begin
-            end
-            sessions {
-              title
-              description
-              type
-              speakers {
-                name
-                image {
-                  url
-                }
-              }
-              location {
-                name
-                city
-                address
-              }
-            }
-          }
-        }
-      }
-    }
-    `;
-  }
-
-  return query;
 }
 
 export default routeGraphQL;
