83ed3c3a0a4d4893bc8a59ce5ccf89c1adceadcd
Juho Vepsalainen <bebraw@gmail.com>
2019-06-12 10:34:04 +0200

    chore: Add Nik to schedule

--- content/react-finland-2020/talks/index.ts
+++ content/react-finland-2020/talks/index.ts
@@ -1,3 +1,4 @@
 export { default as davidKhourshid } from "./david-khourshid";
 export { default as jaredPalmer } from "./jared-palmer";
+export { default as nikGraf } from "./nik-graf";
 export { default as rachelNabors } from "./rachel-nabors";
