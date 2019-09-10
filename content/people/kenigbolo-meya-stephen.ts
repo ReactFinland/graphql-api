6097bf298333244a5c25ea29c3d41520cfa77e86
Kenigbolo Meya Stephen <expensivestephen@hotmail.com>
2019-09-10 21:05:30 +0300

    Update kenigbolo-meya-stephen.ts (#105)

--- content/people/kenigbolo-meya-stephen.ts
+++ content/people/kenigbolo-meya-stephen.ts
@@ -4,7 +4,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "Kenigbolo Meya Stephen",
   about:
-    "Full Stack Software Engineer with a passion for clean code. Working languages are Ruby, JavaScript, Python. Frontend Engineering Team Lead at BCaster",
+    "Stephen is a Frontend Engineering Lead at BCaster and doubles as the Arch Conveyer/Community Manager of CodeAfrique, a not-for-profit weekend intensive software engineering Bootcamp for underrepresented groups in IT which was first started off in Estonia. During my free time, I sometimes volunteer as an open-source developer for https://adsbexchange.com, play golf and coach football (soccer as Americans call it).",
   image: {
     url: "people/kenigbolo.jpg",
   },
