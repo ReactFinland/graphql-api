5ae5d92c1a840eaceaf6afdb3d975eee6bc7238e
Juho Vepsalainen <bebraw@gmail.com>
2019-10-23 09:58:44 +0200

    chore: Add talk description

--- content/react-finland-2020/talks/vivek-nayyar.ts
+++ content/react-finland-2020/talks/vivek-nayyar.ts
@@ -3,8 +3,12 @@ import speaker from "../../people/vivek-nayyar";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: "",
+  title: "Upgrading your legacy code incrementally with codemods",
+  description: `
+How many times has it happened that a new version of a library is released and some of the APIs have either been deprecated or been marked unsafe or at times removed completely. If you have upgraded React from v14 to v16 or styled-components, you understand the issue. JavaScript itself changes over time as well and ideally you'll adapt to the improvements made to the language.
+
+The ongoing change is a challenge to web developers and in this talk we'll focus on how to use JavaScript itself to keep a codebase up to date by using the power of codemods. We'll use an open source framework, jscodeshift, to accomplish this and we'll go through their pros/cons as we become accustomed to their usage.
+  `,
   type: SessionType.TALK,
   keywords: [],
 };
