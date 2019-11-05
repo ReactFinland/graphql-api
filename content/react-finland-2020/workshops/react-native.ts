65773049b0aaff25987b3eeaa1e8de674858d602
Juho Vepsalainen <bebraw@gmail.com>
2019-11-05 09:43:17 +0100

    chore: Update description

--- content/react-finland-2020/workshops/react-native.ts
+++ content/react-finland-2020/workshops/react-native.ts
@@ -1,12 +1,14 @@
 import { Session, SessionType } from "../../../server/schema/Session";
 import { paasitorni } from "../../locations";
-import { naderDabit, juhaLinnanen } from "../../people";
+import { juhaLinnanen, naderDabit } from "../../people";
 
 const workshop: Session = {
   type: SessionType.WORKSHOP,
   people: [naderDabit, juhaLinnanen],
   title: "React Native",
-  description: ``,
+  description: `In this workshop you will learn everything needed to create cross-platform mobile applications for iOS and Android. We will show you how to leverage your possible existing Javascript and React skills and put them in good use creating beautiful mobile apps. Workshop will cover the core features of React Native, which will give you a solid foundation to work on your own afterwards. The workshop will be hands on, creating an example application, where you get to learn first hand the concepts being taught, all the while having experienced trainers ready to help you progress fast. 
+ 
+Pre-requisites: Basic Javascript (ES6) / React knowledge, macOS preferred (ability to build for iOS), tools installed`,
   keywords: [],
   location: paasitorni,
 };
