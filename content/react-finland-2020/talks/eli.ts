9523fec997ae1515f8b9a741e3cad08a806b86de
Eli Schutze <elischutze@gmail.com>
2019-12-16 18:04:57 +0000

    Update eli.ts (#110)

--- content/react-finland-2020/talks/eli.ts
+++ content/react-finland-2020/talks/eli.ts
@@ -3,8 +3,8 @@ import speaker from "../../people/eli";
 
 const talk: Session = {
   people: [speaker],
-  title: "",
-  description: "",
+  title: "What is the deal with i18n?",
+  description: "While about 51% of the world’s websites are in English, only about 25% of web users are English speakers. With half of the world’s population online, internationalisation is as relevant as ever. Let’s explore what i18n means, how it gets made on the web and what we should keep in mind or avoid when making the web more accessible to the world.",
   type: SessionType.TALK,
   keywords: [],
 };
