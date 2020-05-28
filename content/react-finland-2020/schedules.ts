dd19c88e063a06fb23c7fdf7b4ac2f77dc04998f
Juho Vepsäläinen <bebraw@gmail.com>
2020-05-28 17:41:06 +0300

    chore: Update schedule

--- content/react-finland-2020/schedules.ts
+++ content/react-finland-2020/schedules.ts
@@ -85,18 +85,137 @@ const miniConferenceDay: Schedule = {
   intervals: [
     {
       begin: "17:00",
-      end: "21:00",
+      end: "17:15",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "Preparation",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "17:15",
+      end: "17:40",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "Fireside chat",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "17:40",
+      end: "17:45",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "Introduction",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "17:45",
+      end: "18:15",
       sessions: [
         {
           type: SessionType.TALK,
-          title: "First mini-conference about documentation",
-          description:
-            "In the first mini-conference, we'll cover documentation from different angles",
+          title: "Humanizing Your Documentation",
+          description: "",
+          people: [people.carolynStransky],
+        },
+      ],
+    },
+    {
+      begin: "18:15",
+      end: "18:30",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "QA",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "18:30",
+      end: "19:00",
+      sessions: [
+        {
+          type: SessionType.TALK,
+          title: "Documentation in legacy react code - A love/hate paradigm",
+          description: "",
+          people: [people.kenigboloMeyaStephen],
+        },
+      ],
+    },
+    {
+      begin: "19:00",
+      end: "19:15",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "QA",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "19:15",
+      end: "19:45",
+      sessions: [
+        {
+          type: SessionType.TALK,
+          title: "Going beyond Redux. Diversify frontend state management.",
+          description: "",
+          people: [people.margarita],
+        },
+      ],
+    },
+    {
+      begin: "19:45",
+      end: "20:00",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "QA",
+          description: "",
+          people: [],
+        },
+      ],
+    },
+    {
+      begin: "20:00",
+      end: "20:30",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "Panel",
+          description: "",
           people: [
             people.carolynStransky,
             people.kenigboloMeyaStephen,
             people.margarita,
           ],
+        },
+      ],
+    },
+    {
+      begin: "20:30",
+      end: "21:00",
+      sessions: [
+        {
+          type: SessionType.ORGANIZATIONAL,
+          title: "Wrap up",
+          description: "",
+          people: [],
         },
       ],
     },
