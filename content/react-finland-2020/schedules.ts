18850c39bf714713e045ba07e3499d07d3836f7e
Juho Vepsäläinen <bebraw@gmail.com>
2020-08-31 16:29:45 +0200

    chore: Improve formatting

--- content/react-finland-2020/schedules.ts
+++ content/react-finland-2020/schedules.ts
@@ -280,17 +280,18 @@ const miniConferenceDay2: Schedule = {
         {
           type: SessionType.TALK,
           title: "Design from the dimension of open-source",
-          description: `"Getting into an open-source can be quite intimidating as a designer. It’s not something that you can just wake up in the morning and start doing if you’ve never been a designer before. From a high level, let’s “design” our first open-source project, complete with quick methodologies, tips, tricks, and some extra bits.
-          This talk was crafted with the aim of lowering some of the barriers for designers to contribute to open-source, How designers can actually contribute to open-source “live-designing” and providing some direction and resources to help people get started with contributing to design from a realm of open-source.
+          description: `Getting into an open-source can be quite intimidating as a designer. It’s not something that you can just wake up in the morning and start doing if you’ve never been a designer before. From a high level, let’s “design” our first open-source project, complete with quick methodologies, tips, tricks, and some extra bits.
 
-          Key Take-Aways:
+This talk was crafted with the aim of lowering some of the barriers for designers to contribute to open-source, How designers can actually contribute to open-source “live-designing” and providing some direction and resources to help people get started with contributing to design from a realm of open-source.
 
-          - How not to be intimidated as a designer in open-source.
-          - Tools to enable designers to contribute to OSS.
-          - How to inspect design.
-          - Designing and implementing a feature to production as a designer on an enterprise product.
-          - Getting started with designing for designers.
-          - Building/joining a Designers Community as a Designer contributing to open-source."`,
+**Key Take-Aways:**
+
+- How not to be intimidated as a designer in open-source.
+- Tools to enable designers to contribute to OSS.
+- How to inspect design.
+- Designing and implementing a feature to production as a designer on an enterprise product.
+- Getting started with designing for designers.
+- Building/joining a Designers Community as a Designer contributing to open-source.`,
           people: [firstSpeaker],
         },
       ],
@@ -314,10 +315,9 @@ const miniConferenceDay2: Schedule = {
         {
           type: SessionType.TALK,
           title: "Building React UIs visually",
-          description: `"
-          Design tools are fast and amazing tools for thought, but for a host of reasons are limited to creating drawings rather than production assets. Engineers must instead re-create surfaces from scratch, by hand, using code. This inevitably leads to discrepancies and back-and-forth with the design team, and ultimately two sources of truth that are never truly in sync.
-          
-          Plasmic is a tool to build UIs visually, currently in heavy development. It loosely resembles a design tool and aims to give the same sense of speedy iteration, but is for building maintainable, production-ready presentational components. The idea is to give developers a better and faster development experience, eliminate an entire class of visual bugs/QA/tooling, and ultimately maintain a single source of truth with design. This talk will also cover some of the challenges with building such a tool (which itself is a React application)."`,
+          description: `Design tools are fast and amazing tools for thought, but for a host of reasons are limited to creating drawings rather than production assets. Engineers must instead re-create surfaces from scratch, by hand, using code. This inevitably leads to discrepancies and back-and-forth with the design team, and ultimately two sources of truth that are never truly in sync.
+
+Plasmic is a tool to build UIs visually, currently in heavy development. It loosely resembles a design tool and aims to give the same sense of speedy iteration, but is for building maintainable, production-ready presentational components. The idea is to give developers a better and faster development experience, eliminate an entire class of visual bugs/QA/tooling, and ultimately maintain a single source of truth with design. This talk will also cover some of the challenges with building such a tool (which itself is a React application).`,
           people: [secondSpeaker],
         },
       ],
