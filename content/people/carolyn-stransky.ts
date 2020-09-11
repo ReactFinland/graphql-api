f152079babb795389a545c7abd50ff444f0bc77b
Carolyn Stransky <hello@workwithcarolyn.com>
2020-09-11 19:26:35 +0200

    Fix typo in Carolyn's bio (#119)

--- content/people/carolyn-stransky.ts
+++ content/people/carolyn-stransky.ts
@@ -4,7 +4,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "Carolyn Stransky",
   about:
-    "Carolyn Stransky (she/her) is a software developer and journalist based in Berlin, Germany. Her work focuses on creating acccessible, user-centric interfaces. Out of the office, she tries to convince everyone that documentation is cool while also maintaining an admittedly ridiculous skincare routine. You can find her most places on the Internet @carolstran.",
+    "Carolyn Stransky (she/her) is a software developer and journalist based in Berlin, Germany. Her work focuses on creating accessible, user-centric interfaces. Out of the office, she tries to convince everyone that documentation is cool while also maintaining an admittedly ridiculous skincare routine. You can find her most places on the Internet @carolstran.",
   image: {
     url: "people/carolyn.jpg",
   },
