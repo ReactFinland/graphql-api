e5c100c51d5c597955e43d0a4faf175e2aebdda1
Juho Vepsäläinen <bebraw@gmail.com>
2020-11-09 16:17:39 +0100

    chore: Fix formatting

--- content/people/andrey-g.ts
+++ content/people/andrey-g.ts
@@ -3,7 +3,8 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Andrey Goncharov",
-  about: "Andrey is a full-stack TypeScript and JavaScript developer. He came from working in two software consultancies on a variety of projects: from small startups to well-established enterprises. Now he is happy to be a part of a family at Hazelcast. Big lover of open-source. Author of a small technical blog. Runs BeerJS Voronezh.",
+  about:
+    "Andrey is a full-stack TypeScript and JavaScript developer. He came from working in two software consultancies on a variety of projects: from small startups to well-established enterprises. Now he is happy to be a part of a family at Hazelcast. Big lover of open-source. Author of a small technical blog. Runs BeerJS Voronezh.",
   image: {
     url: "people/andrey-g.jpg",
   },
