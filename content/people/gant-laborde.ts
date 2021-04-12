a9f9611d99e508b03e7dca15b78304dca1efa8e1
Juho Vepsäläinen <bebraw@gmail.com>
2021-04-12 14:43:24 +0300

    fix: Add an image for Gant

--- content/people/gant-laborde.ts
+++ content/people/gant-laborde.ts
@@ -6,7 +6,7 @@ const person: Contact = {
   about:
     "Gant is a New Orleans-based force of nature. Not satisfied to be “just” an experienced programmer in an array of disciplines, he’s also a prolific writer, an award-winning public speaker, and an adjunct professor.\n\nA self-improvement aficionado, he takes every opportunity to connect with people and share knowledge through education and outreach. In the zombie apocalypse, Gant will run our much-needed bar, offering up advice, encouragement, and libations in equal measure.",
   image: {
-    url: "people/gant.png",
+    url: "people/gant.jpg",
   },
   social: {
     homepage: "http://gantlaborde.com/",
