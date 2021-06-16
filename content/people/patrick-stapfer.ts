948826f37af3fa7383c3c7ae044f659ff4f0f04b
Juho Vepsäläinen <bebraw@gmail.com>
2021-06-16 11:26:49 +0300

    chore: Update info

--- content/people/patrick-stapfer.ts
+++ content/people/patrick-stapfer.ts
@@ -2,9 +2,9 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
-  name: "Patrick Stapfer",
+  name: "Patrick Ecker",
   about:
-    "Patrick Stapfer is a JavaScript Software Engineer and freelancer, mostly known for his contributions to the Flow type checker ecosystem and his recent work at Runtastic, where he spent most of his time working on the Mobile News Feed feature (built in React Native). Most of his Open Source time is dedicated to the ReasonML and OCaml platform to build robust, type-safe JS applications.",
+    "Patrick is a JavaScript Software Engineer and freelancer, mostly known for his contributions to the Flow type checker ecosystem and his recent work at Runtastic, where he spent most of his time working on the Mobile News Feed feature (built in React Native). Most of his Open Source time is dedicated to the ReasonML and OCaml platform to build robust, type-safe JS applications.",
   image: {
     url: "people/patrick.jpg",
   },
