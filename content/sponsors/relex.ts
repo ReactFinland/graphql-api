333d8ec9e51baf2deddec1aa4141f568da1db98c
Juho Vepsäläinen <bebraw@gmail.com>
2021-06-22 16:15:01 +0300

    chore: Update sponsors

---
+++ content/sponsors/relex.ts
@@ -0,0 +1,25 @@
+import { Contact, ContactType } from "../../server/schema/Contact";
+
+const sponsor: Contact = {
+  name: "Relex",
+  about: "Optimize retail for every future",
+  image: {
+    url: "sponsors/relex.svg",
+  },
+  social: {
+    homepage: "https://www.relexsolutions.com/",
+    facebook: "RELEXSolutions",
+    twitter: "RelexSolutions",
+    linkedin: "relexsolutions",
+  },
+  location: {
+    country: {
+      name: "Finland",
+      code: "FI",
+    },
+    city: "Helsinki",
+  },
+  type: [ContactType.SPONSOR],
+};
+
+export default sponsor;
