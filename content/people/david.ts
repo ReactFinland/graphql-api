82703bf2ddbbc7942bcd97510b4b5416b6e62f1c
David Leitner <david.leitner@squer.at>
2021-06-24 16:17:17 +0200

    Update bio of David Leitner

--- content/people/david.ts
+++ content/people/david.ts
@@ -4,7 +4,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "David Leitner",
   about:
-    "Enthusiastic Software Professional from Vienna. I don't sleep too much, do open source and prefer keeping my code simple and small instead of clever and edgy.",
+    "David is Coding Architect at SQUER, a Viennese Software Company, which is working with different stacks and environments, but always an overarching mission: connect ideas and provide impact — with technology. He spends much of his time on the frontlines tackling the challenges of scaling software and complex domains, with a strong focus on reactive systems, cloud-native architectures and state of the art web technologies. David enjoys sharing his knowledge as a speaker at conferences and as a lecturer for his post-diploma courses at the University of Applied Sciences Technikum Vienna.",
   image: {
     url: "people/david-l.jpg",
   },
