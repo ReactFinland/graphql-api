8bc3be74942101380d100cbc1be4357366f57631
Juho Vepsäläinen <bebraw@gmail.com>
2021-06-30 10:09:31 +0300

    chore: Apply formatting

--- content/people/eevis.ts
+++ content/people/eevis.ts
@@ -3,7 +3,8 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Eeva-Jonna 'Eevis' Panula",
-  about: "Eevis is a software developer and accessibility specialist based in Finland. She loves sharing information about topics like accessibility, React and GraphQL, and is active in different communities aiming to increase equality in the tech field in Finland. When not coding, she explores the world around her with a kayak. ",
+  about:
+    "Eevis is a software developer and accessibility specialist based in Finland. She loves sharing information about topics like accessibility, React and GraphQL, and is active in different communities aiming to increase equality in the tech field in Finland. When not coding, she explores the world around her with a kayak. ",
   image: {
     url: "people/eevis.jpg",
   },
