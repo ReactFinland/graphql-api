a384439f51cc6c62a326b4f7bcf66d7052804cbe
Juho Vepsäläinen <bebraw@gmail.com>
2021-07-09 12:56:22 +0300

    chore: Add missing data

--- content/people/juri.ts
+++ content/people/juri.ts
@@ -2,8 +2,9 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
-  name: "",
-  about: "",
+  name: "Juri Strumpflohner",
+  about:
+    "I've been working as a software developer and architect for 10+ years, with technologies like Java, .Net, Node.js on the desktop, mobile as well as web. Lately I'm mostly focusing on frontend web development with JavaScript, on writing technical articles, teaching and consulting.",
   image: {
     url: "people/juri.jpg",
   },
