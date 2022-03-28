9f1ebc980d3f7a662073cf07f6e442f3f1f2e6ee
Juho Vepsäläinen <bebraw@gmail.com>
2022-03-28 10:58:42 +0300

    chore: Tune description

--- content/people/david-khourshid.ts
+++ content/people/david-khourshid.ts
@@ -4,7 +4,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "David Khourshid",
   about:
-    "David Khourshid is a Florida-based web developer for Microsoft, a tech author, and speaker. Also a fervent open-source contributor, he is passionate about JavaScript, CSS, animation, innovative user interfaces, and cutting-edge front-end technologies. When not behind a computer keyboard, he’s behind a piano keyboard or traveling.",
+    "David Khourshid is a Florida-based founder of Stately, a tech author, and speaker. Also a fervent open-source contributor, he is passionate about JavaScript, CSS, animation, innovative user interfaces, and cutting-edge front-end technologies. When not behind a computer keyboard, he’s behind a piano keyboard or traveling.",
   image: {
     url: "people/david.jpg",
   },
