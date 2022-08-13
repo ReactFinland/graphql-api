0bfb26b5157ffc71bfc7ba1b50700b0d0b2fbed4
Esa-Matti Suuronen <github@esamatti.fi>
2022-08-13 12:10:11 +0300

    Update talk and bio for Esa-Matti (#171)

--- content/people/esa-matti.ts
+++ content/people/esa-matti.ts
@@ -3,12 +3,13 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Esa-Matti Suuronen",
-  about: "",
+  about:
+    "Esa-Matti is a software developer from Jyväskylä, Finland with more than 10 years of professional software development experience who has used React and Node.js since their 0.x days. Currently working for Valu Digital as a Lead Developer building developer tools.",
   image: {
     url: "people/esa-matti.jpg",
   },
   social: {
-    homepage: "",
+    homepage: "https://esamatti.fi/",
     twitter: "esamatti",
     github: "esamattis",
     facebook: "",
