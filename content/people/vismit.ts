e53b3208292948ea99da6299c1f959e6e431422d
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-15 11:37:37 +0300

    chore: Update bio

--- content/people/vismit.ts
+++ content/people/vismit.ts
@@ -3,7 +3,8 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Vismit Chawla",
-  about: "Talent Acquisition Specialist at RELEX Solutions.",
+  about:
+    "Vismit is an experienced recruitment professional with over 10 years of recruiting background in Indian, Danish and Finnish market. Outside work, she likes meeting people with diverse cultural background to enhance on learning new personality & culture and love following healthy lifestyle focusing on nutrition. Passionate about DEI topics and mentoring international talents in integrating in Finnish job market.",
   image: {
     url: "people/vismit.jpg",
   },
