d9b7c24ca3a64dee4f4d11a3f0a8f9dd9b2c170f
Juho Vepsäläinen <bebraw@gmail.com>
2022-08-26 15:29:52 +0300

    chore: Update wording

--- content/people/laurao.ts
+++ content/people/laurao.ts
@@ -4,7 +4,7 @@ import Keyword from "../../server/schema/keywords";
 const person: Contact = {
   name: "Laura Ojala",
   about:
-    "Laura Ojala (they/she) has over 15 years of experience in various agile testing roles from Quality Manager in Medical Device start-up to Test Manager in the Financial sector. Since early 2020 they’ve been a Senior Test Automation Developer at Oura Health, the company behind the Oura ring. Laura enjoys creative challenges and supporting others on their journey. One of their passions is Creative Group Process Facilitation.",
+    "Laura Ojala (they/she) has over 15 years of experience in various agile testing roles from Quality Manager in Medical Device start-up to Test Manager in the Financial sector. Since early 2020 they’ve been a Senior Test Automation Developer. Laura enjoys creative challenges and supporting others on their journey. One of their passions is Creative Group Process Facilitation.",
   image: {
     url: "people/laurao.jpg",
   },
