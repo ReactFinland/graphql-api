1de3a9dd7f36f9939d058bf7e9773d4be0666fc1
Juho Vepsäläinen <bebraw@gmail.com>
2022-12-15 15:06:14 +0200

    chore: Improve description

--- content/people/thais-santos.ts
+++ content/people/thais-santos.ts
@@ -3,7 +3,7 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Thais Santos",
-  about: `Principal Developer Advocate at ‹div›RIOTS - makers of Backlight, a Design SDesign Systems crusader | Blockchain enthusiast | Design Lead @divriots | Making the IT world better one component at a time`,
+  about: `Principal Developer Advocate at [‹div›RIOTS](https://divriots.com/) - makers of [Backlight](https://backlight.dev/), a Design System creation system. Design Systems crusader | Blockchain enthusiast | Design Lead @divriots | Making the IT world better one component at a time`,
   image: {
     url: "people/thais.jpg",
   },
