9d09edd829df8b4aa7ca86479d0fca6a8259e7db
Juho Vepsäläinen <bebraw@gmail.com>
2023-01-18 12:11:26 +0200

    chore: Improve formatting

--- content/people/tero-parviainen.ts
+++ content/people/tero-parviainen.ts
@@ -3,8 +3,7 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "Tero Parviainen",
-  about:
-    "Software developer, music hacker. One half of Counterpoint.",
+  about: "Software developer, music hacker. One half of Counterpoint.",
   image: {
     url: "people/tero.jpg",
   },
