7e4569319b0ce6ad079a0e997d19fb500a71f5d0
Juho Vepsäläinen <bebraw@gmail.com>
2023-02-13 20:37:29 +0200

    chore: Drop usage of util

--- server/logger.ts
+++ server/logger.ts
@@ -1,7 +1,6 @@
 // https://github.com/amazeeio/lagoon/blob/master/services/api/src/logger.js
 import * as fs from "fs";
 import * as path from "path";
-import { isString } from "util";
 import { createLogger, format, transports } from "winston";
 
 // Create the logs directory if it doesn't exist yet.
@@ -10,12 +9,12 @@ if (!fs.existsSync(directory)) {
   fs.mkdirSync(directory);
 }
 
-const ignorePingAndMedia = format(info => {
+const ignorePingAndMedia = format((info) => {
   const message = info.message;
 
   if (
-    isString(message) &&
-    ["GET /ping ", "GET /media"].some(match => message.includes(match))
+    typeof message === "string" &&
+    ["GET /ping ", "GET /media"].some((match) => message.includes(match))
   ) {
     return false;
   }
