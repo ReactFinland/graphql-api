b8fcb80ab14be2557801ffebe0ef1bf06217066d
Juho Vepsäläinen <bebraw@gmail.com>
2023-02-13 20:28:53 +0200

    chore: Expose ping anyway

--- server/routes/ping.ts
+++ server/routes/ping.ts
@@ -1,9 +1,7 @@
 function routePing(router) {
-  if (process.env.NODE_ENV !== "production") {
-    const serverVersion = new Date().toString();
+  const serverVersion = new Date().toString();
 
-    router.get("/ping", (req, res) => res.status(200).json({ serverVersion }));
-  }
+  router.get("/ping", (req, res) => res.status(200).json({ serverVersion }));
 }
 
 export default routePing;
