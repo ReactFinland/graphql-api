3b7380c8755e894f38b82d9087688006d0308299
Juho Vepsäläinen <bebraw@gmail.com>
2023-05-02 10:38:14 +0300

    chore: Update name

--- content/people/thais-santos.ts
+++ content/people/thais-santos.ts
@@ -2,7 +2,7 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
-  name: "Thais Santos",
+  name: "Thaís Santos",
   about: `Design Systems crusader 🧭 Currently lead design at [‹div›RIOTS](https://divriots.com/) - makers of [Backlight](https://backlight.dev/): a unique, all-in-one design system solution.`,
   image: {
     url: "people/thais.jpg",
