4ed2771ff37bea490b4effa69294e0132283fc5a
Juho Vepsäläinen <bebraw@gmail.com>
2023-06-01 10:01:00 +0300

    fix: Fix a typo

--- content/future-frontend-2023/schedules.ts
+++ content/future-frontend-2023/schedules.ts
@@ -160,7 +160,7 @@ const wednesday: Schedule = {
           title: "Building Fast WebApps with QwikCity",
           description: `Building instant-on web applications at scale have been elusive. Real-world sites need tracking, analytics, and complex user interfaces and interactions. We always start with the best intentions but end up with a less-than-ideal site.
 
-QwikCity is a new meta-framework that allows you to build large-scale applications with constant startup-up performance. We will look at how to build a QwikCity application and what makes it unique. The workshop will show you how to set up a QwikCitp project. How routing works with layout. The demo application will fetch data and present it to the user in an editable form. And finally, how one can use authentication. All of the basic parts for any large-scale applications.
+QwikCity is a new meta-framework that allows you to build large-scale applications with constant startup-up performance. We will look at how to build a QwikCity application and what makes it unique. The workshop will show you how to set up a QwikCity project. How routing works with layout. The demo application will fetch data and present it to the user in an editable form. And finally, how one can use authentication. All of the basic parts for any large-scale applications.
 
 Along the way, we will also look at what makes Qwik unique, and how resumability enables constant startup performance no matter the application complexity.`,
           location: locations.columbiaroad,
