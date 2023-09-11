85df8125cc59879c004afbae644f789ccb80f960
Juho Vepsäläinen <bebraw@gmail.com>
2023-09-11 11:48:14 +0300

    chore: Add workshops

--- content/future-frontend-2024/schedules.ts
+++ content/future-frontend-2024/schedules.ts
@@ -26,6 +26,30 @@ const tuesday: Schedule = {
         },
       ],
     },
+    {
+      begin: "06:00",
+      end: "14:00",
+      sessions: [
+        {
+          type: SessionType.WORKSHOP,
+          people: [people.teroParviainen, people.juhoVepsalainen],
+          title: "Web Audio from zero to DAW",
+          description: `This full-day workshop is for current or aspiring musicians, or people who are interested in synthesis in general. After the workshop, you will understand the fundamentals of synthesis, web audio basics, and some of the newest modern web Audio features. Expect to learn about the Web Audio API, Audio Worklets, Synthesis, Music Theory, Audio focused UI and visualization, and sequencing and composition.`,
+          // location: locations.nitor,
+        },
+        {
+          type: SessionType.WORKSHOP,
+          people: [people.misko],
+          title: "Building Fast WebApps with QwikCity",
+          description: `Building instant-on web applications at scale have been elusive. Real-world sites need tracking, analytics, and complex user interfaces and interactions. We always start with the best intentions but end up with a less-than-ideal site.
+
+QwikCity is a new meta-framework that allows you to build large-scale applications with constant startup-up performance. We will look at how to build a QwikCity application and what makes it unique. The workshop will show you how to set up a QwikCity project. How routing works with layout. The demo application will fetch data and present it to the user in an editable form. And finally, how one can use authentication. All of the basic parts for any large-scale applications.
+
+Along the way, we will also look at what makes Qwik unique, and how resumability enables constant startup performance no matter the application complexity.`,
+          // location: locations.columbiaroad,
+        },
+      ],
+    },
   ],
 };
 
