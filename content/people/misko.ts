07c3bb65766594e6e1a39b423257f321b6af069c
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-16 09:08:05 +0200

    fix: Fix a typo

--- content/people/misko.ts
+++ content/people/misko.ts
@@ -2,7 +2,7 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 
 const person: Contact = {
   name: "Miško Hevery",
-  about: `As CTO, Miško oversees the technology division that powers the Builder.io applications and software. Before joining Builder.io, he created Open Source platforms for Google, including Angular, AngularJS and was co-creator of Karma. While at Google, he brought a testing culture there with his blog. Before focusing on making the web better, he believes testing is the key to success.
+  about: `As a CTO, Miško oversees the technology division that powers the Builder.io applications and software. Before joining Builder.io, he created Open Source platforms for Google, including Angular, AngularJS and was co-creator of Karma. While at Google, he brought a testing culture there with his blog. Before focusing on making the web better, he believes testing is the key to success.
 
   Miško started his career designing digital circuits and moved to databases, full-stack development and finally, front-end frameworks, giving him a unique perspective. He understands all of the layers from the web down to a transistor. In addition to Google, he worked for tech powerhouses Adobe Systems and Sun Microsystems.
 
