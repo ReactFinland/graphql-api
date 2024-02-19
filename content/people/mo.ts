42b23d8ff6594f9f25ca30b830a1dc85e584f587
Juho Vepsäläinen <bebraw@gmail.com>
2024-02-19 13:20:19 +0200

    chore: Add a twitter handle

--- content/people/mo.ts
+++ content/people/mo.ts
@@ -10,7 +10,7 @@ const person: Contact = {
   social: {
     homepage: "",
     mastodon: "", // This has to be a whole link
-    twitter: "", // Handle without @ is enough
+    twitter: "mo__javad", // Handle without @ is enough
     github: "",
     facebook: "",
     medium: "",
