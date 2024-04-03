e8f3852fb48e9a4900650d80d6cf602872b0f772
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-03 16:03:18 +0300

    fix: Fix a typo

--- content/people/uj.ts
+++ content/people/uj.ts
@@ -2,7 +2,8 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 
 const person: Contact = {
   name: "Ujjwal Sharma",
-  about: "Ujjwal is a Compilers Hacker at Igalia working on TC39 and V8, a Node.js Core Collaborator, a TC39 Delegate and an International Speaker. He loves to talk about standards and about open source software, decentralization, cryptography, JavaScript and web".
+  about:
+    "Ujjwal is a Compilers Hacker at Igalia working on TC39 and V8, a Node.js Core Collaborator, a TC39 Delegate and an International Speaker. He loves to talk about standards and about open source software, decentralization, cryptography, JavaScript and web",
   image: {
     url: "people/uj.jpeg",
   },
