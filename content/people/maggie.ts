ca1f7046d792206fbd15580ab14fb26500ef85bf
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-04 14:34:36 +0300

    chore: Add content

--- content/people/maggie.ts
+++ content/people/maggie.ts
@@ -3,7 +3,11 @@ import { Contact } from "../../server/schema/Contact";
 
 const person: Contact = {
   name: "Maggie Appleton",
-  about: "",
+  about: `I sit at the intersection of design, anthropology, and programming. These three are at the core of everything I make. Combining them into a coherent career is a weird and ongoing challenge.
+
+Titles and disciplines are fickle and fleeting. But my work fits under the umbrellas of UX design, visual interface design, and DX (developer experience). With some cultural analysis, writing, and visual illustration sprinkled on top.
+
+I currently lead design atElicitwhere we're exploring how machine learning can help researchers with open-ended reasoning.`,
   image: {
     url: "people/maggie.jpeg",
   },
