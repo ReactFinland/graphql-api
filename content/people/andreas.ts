0ab11ef49150ceb601b08fdc2744f8c9c596e3ce
Vakisr <vakisr@gmail.com>
2024-05-15 11:23:37 +0200

    Update andreas.ts
    Added a new bio for Andreas

--- content/people/andreas.ts
+++ content/people/andreas.ts
@@ -4,7 +4,7 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 const person: Contact = {
   name: "Andreas Møller",
   about:
-    "Experienced Vice President Of Engineering with a demonstrated history of working in the information technology and services industry.",
+    "Andreas Møller is a software engineer with 20+ years of experience. He is a Fellow of the British Computer Society and does not think text editors are the best way to build web apps. He is the CEO and co-founder of [toddle](https://www.toddle.dev/) - the design collaboration tool for developers and designers.",
   image: {
     url: "people/andreas.jpeg",
   },
