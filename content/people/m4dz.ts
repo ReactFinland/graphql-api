7cbece4bd31c2ba946cf1e41593a71a0437cd690
Juho Vepsäläinen <bebraw@gmail.com>
2024-09-06 11:35:37 +0300

    chore: Update speaker info

--- content/people/m4dz.ts
+++ content/people/m4dz.ts
@@ -3,7 +3,7 @@ import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
   name: "m4dz 🎙🥑",
-  about: `Principal Developer Advocate at ‹div›RIOTS - makers of Backlight, a Design Systems IDE built for Designers and Developers - m4dz is a curious animal. Former Frontend Dev, Devtools enthusiast, he has made the Developer eXperience (DX) his cherished topic. All the Web technologies are under his radar to get things ever simpler. His moto: "Developers are users just like you." As a life-long dreamer his favorite book will ever remains "Alice's Adventures in Wonderland".`,
+  about: `CTO at Zenika. m4dz is a curious animal. Former Frontend Dev, Devtools enthusiast, he has made the Developer eXperience (DX) his cherished topic. All the Web technologies are under his radar to get things ever simpler. His moto: "Developers are users just like you." As a life-long dreamer his favorite book will ever remains "Alice's Adventures in Wonderland".`,
   image: {
     url: "people/m4dz.jpg",
   },
@@ -15,7 +15,7 @@ const person: Contact = {
     medium: "",
     instagram: "",
     linkedin: "m4d-z",
-    youtube: "https://www.youtube.com/c/Backlight_design_systems",
+    youtube: "",
     devto: "",
   },
   location: {
