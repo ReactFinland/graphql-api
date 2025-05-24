4f415094700d585b0cd4cd8d7d2d41be5a5b5e44
Juho Vepsäläinen <bebraw@gmail.com>
2025-05-24 07:10:46 +0300

    fix: Fix name

--- content/people/rachel-nabors.ts
+++ content/people/rachel-nabors.ts
@@ -2,7 +2,7 @@ import { Contact, ContactType } from "../../server/schema/Contact";
 import Keyword from "../../server/schema/keywords";
 
 const person: Contact = {
-  name: "Rachel Nabors",
+  name: "Rachel-Lee Nabors",
   about: `Rachel-Lee Nabors spent the better part of their career on web standards and opensource and has spearheaded developer education at FAANG and startups, on the React Team, and W3C. Now they work to usher in the future with browser builders and Silicon Valley startups, teaching a new generation of builders that “it's not magic; it's just math.” You can find them drinking tea in London or shadowboxing in San Francisco.`,
   image: {
     url: "people/rachel.jpeg",
