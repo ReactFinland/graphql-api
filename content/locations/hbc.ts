dbe40734aa4ced926ea35cbb4575a5be054ce012
Juho Vepsäläinen <bebraw@gmail.com>
2026-02-11 11:56:39 +0200

    chore: Add info

--- content/locations/hbc.ts
+++ content/locations/hbc.ts
@@ -2,7 +2,7 @@ import { Location } from "../../server/schema/Location";
 
 // FIXME: These should be Contacts instead -> Location can become simpler again
 const location: Location = {
-  name: "Haaga-Helia",
+  name: "Haaga-Helia University of Applied Sciences - Pasila campus",
   about: "Haaga-Helia (Helsinki) is one of the workshop hosts of the event.",
   social: {
     homepage: "https://www.haaga-helia.fi/",
