1a286bfb652ec79642b2bffe108f894115f92e6a
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-03 13:15:28 +0300

    Upgrade GraphQL dependencies

--- server/schema/index.ts
+++ server/schema/index.ts
@@ -1,4 +1,4 @@
-import { buildSchema, registerEnumType } from "type-graphql";
+import { buildSchema, registerEnumType, type NonEmptyArray } from "type-graphql";
 import { ContactType } from "./Contact";
 import * as resolvers from "./resolvers";
 import { Level, SessionType } from "./Session";
@@ -19,8 +19,12 @@ registerEnumType(Level, {
 });
 
 export default async function generateSchema() {
+  const schemaResolvers = Object.values(
+    resolvers
+  ) as unknown as NonEmptyArray<Function>;
+
   return await buildSchema({
-    resolvers: Object.values(resolvers),
+    resolvers: schemaResolvers,
     // scalarsMap: [{ type: Url, scalar: UrlScalar }],
     // TODO: Emitting the schema could be useful for query checks.
     // emitSchemaFile: true, // use path for a specific location
