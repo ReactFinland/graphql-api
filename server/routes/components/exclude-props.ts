8d8b89d57fdd08cb0847970ee43e1877e6b73c13
Juho Vepsäläinen <bebraw@gmail.com>
2026-04-03 12:54:56 +0300

    Remove dead template and utility code

--- server/routes/components/exclude-props.ts
+++
@@ -1,9 +0,0 @@
-import omit from "lodash/omit";
-import React from "react";
-
-export default function excludeProps(exclude, element) {
-  const ExcludedStyled = props =>
-    React.createElement(element, omit(props, exclude));
-
-  return ExcludedStyled;
-}
--- server/routes/date-utils.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Use browser here instead?
-function dayToFinnishLocale(day: string): string {
-  const date = new Date(day);
-
-  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-}
-
-export { dayToFinnishLocale };
